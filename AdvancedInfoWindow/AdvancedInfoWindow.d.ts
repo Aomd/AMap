444c7cb189f683391fc5b521e63452f11ff4029e
Aomd <g00665@163.com>
2020-01-02 00:01:12 +0800

    完善 AdvancedInfoWindow

--- AdvancedInfoWindow/AdvancedInfoWindow.d.ts
+++ AdvancedInfoWindow/AdvancedInfoWindow.d.ts
@@ -1,7 +1,113 @@
 import { AdvancedInfoWindowOptions } from "./AdvancedInfoWindowOptions";
+import { LngLat } from "../LngLat";
+import { Map } from "../Map/Map";
 
+
+/**
+ * 用于在地图上弹出一个详细信息展示窗体
+ *
+ * @export
+ * @class AdvancedInfoWindow
+ */
 export class AdvancedInfoWindow {
+
+  /**
+   * Creates an instance of AdvancedInfoWindow.
+   * 构造详细信息展示窗体。
+   * @param {(AdvancedInfoWindowOptions | Object)} opt
+   * @memberof AdvancedInfoWindow
+   */
   constructor(
     opt: AdvancedInfoWindowOptions | Object
   )
+
+
+  /**
+   * 在地图的指定位置打开信息窗体
+   *
+   * @param {Map} map
+   * @param {LngLat} pos
+   * @memberof AdvancedInfoWindow
+   */
+  open(map: Map, pos: LngLat)
+
+
+  /**
+   * 关闭信息窗体
+   *
+   * @memberof AdvancedInfoWindow
+   */
+  close()
+
+
+  /**
+   * 获取信息窗体是否打开
+   *
+   * @returns {Boolean}
+   * @memberof AdvancedInfoWindow
+   */
+  getIsOpen(): Boolean
+
+
+  /**
+   * 设置信息窗体内容，可通过该函数动态更新信息窗体中的信息
+   *
+   * @param {(String | HTMLElement)} content
+   * @memberof AdvancedInfoWindow
+   */
+  setContent(content: String | HTMLElement)
+
+
+  /**
+   *
+   * 获取信息窗体内容 ，结果以字符串方式返回
+   * @returns {String}
+   * @memberof AdvancedInfoWindow
+   */
+  getContent(): String
+
+
+  /**
+   * 设置信息窗体显示基点位置
+   *
+   * @param {LngLat} lnglat
+   * @memberof AdvancedInfoWindow
+   */
+  setPosition(lnglat: LngLat)
+
+
+  /**
+   * 获取信息窗体显示基点位置
+   *
+   * @returns {LngLat}
+   * @memberof AdvancedInfoWindow
+   */
+  getPosition(): LngLat
+
+
+  /**
+   * 获取信息窗体锚点
+   *
+   * @returns {String}
+   * @memberof AdvancedInfoWindow
+   */
+  getAnchor(): String
+
+
+  /**
+   *
+   * 设置信息窗体锚点
+   * 可选值：'top-left'|'top-center'|'top-right'|'middle-left'|'center'|'middle-right'|'bottom-left'|'bottom-center'|'bottom-right'    
+   * @memberof AdvancedInfoWindow
+   */
+  setAnchor()
+
+
+  /**
+   * 清除高级信息窗体在地图上绘制的路线规划或者搜索的结果
+   *
+   * @memberof AdvancedInfoWindow
+   */
+  clear()
+
 }
--- AdvancedInfoWindow/AdvancedInfoWindowOptions.d.ts
+++ AdvancedInfoWindow/AdvancedInfoWindowOptions.d.ts
@@ -1,6 +1,48 @@
-export class AdvancedInfoWindowOptions{
+import { Pixel } from "../Pixel";
+import { LngLat } from "../LngLat";
+
+
+/**
+ *
+ *
+ * @export
+ * @class AdvancedInfoWindowOptions
+ */
+export class AdvancedInfoWindowOptions {
+
+
+  /**
+   *Creates an instance of AdvancedInfoWindowOptions.
+   * @param {Boolean} autoMove 是否自动调整窗体到视野内（当信息窗体超出视野范围时，通过该属性设置是否自动平移地图，使信息窗体完全显示）
+   * @param {Boolean} closeWhenClickMap 控制是否在鼠标点击地图后关闭信息窗体，默认false，鼠标点击地图后不关闭信息窗体
+   * @param {(String | HTMLElement)} content 显示内容，可以是HTML要素字符串或者HTMLElement对象
+   * @param {String} anchor 信息窗体锚点。  默认值：'bottom-center' 可选值：'top-left'|'top-center'|'top-right'|'middle-left'|'center'|'middle-right'|'bottom-left'|'bottom-center'|'bottom-right'（自v1.4.13 新增）
+   * @param {Pixel} offset 信息窗体显示位置偏移量。默认基准点为信息窗体的底部中心（若设置了anchor，则以anchor值为基准点）。
+   * @param {LngLat} position 信息窗体显示基点位置（自v1.2 新增）         
+   * @param {(String | HTMLElement)} panel 结果列表的HTML容器id或容器元素，提供此参数后，结果列表将在此容器中进行展示。可选值
+   * @param {Number} searchRadius 设定周边搜索的半径，默认值：5000，单位：米
+   * @param {Boolean} placeSearch 是否支持显示周边搜索，默认是true
+   * @param {Boolean} driving 是否支持驾车路径规划，默认是true
+   * @param {Boolean} walking 是否支持步行路径规划，默认是true
+   * @param {Boolean} transit 是否支持公交路径规划，默认是true
+   * @param {Boolean} asOrigin 是否支持作为路径规划的起点，默认是true
+   * @param {Boolean} asDestination 是否支持作为路径规划的终点，默认是true
+   * @memberof AdvancedInfoWindowOptions
+   */
   constructor(
-    autoMove:Boolean,
-    
+    autoMove: Boolean,
+    closeWhenClickMap: Boolean,
+    content: String | HTMLElement,
+    anchor: String,
+    offset: Pixel,
+    position: LngLat,
+    panel: String | HTMLElement,
+    searchRadius: Number,
+    placeSearch: Boolean,
+    driving: Boolean,
+    walking: Boolean,
+    transit: Boolean,
+    asOrigin: Boolean,
+    asDestination: Boolean,
   )
 }

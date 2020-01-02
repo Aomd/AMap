6cc4a9fd8650c71425d1de6dd2173ba104cd3e3b
Aomd <g00665@163.com>
2020-01-02 20:28:44 +0800

    完善 Autocomplete

--- Autocomplete/Autocomplete.d.ts
+++ Autocomplete/Autocomplete.d.ts
@@ -1,3 +1,70 @@
-export class Autocomplete{
-  
+import { AutocompleteOptions } from './AutocompleteOptions';
+
+type AutocompleteEventNames = 
+'complete'|
+'error'|
+'select'|
+'choose';
+
+/**
+ * 根据输入关键字提示匹配信息
+ *
+ * @export
+ * @class Autocomplete
+ */
+export class Autocomplete {
+
+    /**
+     * 构造函数，提供输入提示功能
+     *Creates an instance of Autocomplete.
+     * @param {(AutocompleteOptions | Object)} opts
+     * @memberof Autocomplete
+     */
+    constructor(
+        opts: AutocompleteOptions | Object
+    )
+
+
+    /**
+     * 根据输入关键字提示匹配信息，支持中文、拼音
+     * 当status为complete时，result为AutocompleteResult；
+     * 当status为error时，result为错误信息info；
+     * 当status为no_data时，代表检索返回0结果
+     *
+     * @param {String} keyword
+     * @param {Function} callback
+     * @memberof Autocomplete
+     */
+    search(keyword: String, callback: Function)
+
+
+    /**
+     * 设置提示Poi类型，多个类型用“|”分隔，POI相关类型请在网站“相关下载”处下载
+     * 目前只支持Poi类型编码如“050000”
+     * 默认值：所有类别
+     *
+     * @param {String} type
+     * @memberof Autocomplete
+     */
+    setType(type: String)
+
+
+    /**
+     * 设置城市
+     *
+     * @param {String} city
+     * @memberof Autocomplete
+     */
+    setCity(city: String)
+
+
+    /**
+     * 设置是否强制限制城市
+     *
+     * @param {*} Boolean
+     * @memberof Autocomplete
+     */
+    setCityLimit(Boolean)
+
+    on(event: AutocompleteEventNames ,cb:Function)
 }
--- Autocomplete/AutocompleteOptions.d.ts
+++ Autocomplete/AutocompleteOptions.d.ts
@@ -1,3 +1,31 @@
+
 export class AutocompleteOptions{
-  
+
+
+  /**
+   *Creates an instance of AutocompleteOptions.
+   * @param {String} datatype  返回的数据类型可选值：all-返回所有数据类型、poi-返回POI数据类型、bus-返回公交站点数据类型、busline-返回公交线路数据类型目前暂时不支持多种类型
+   * @param {Boolean} citylimit 是否强制限制在设置的城市内搜索,默认值为：falsetrue：强制限制设定城市，false：不强制限制设定城市
+   * @param {(String|HTMLInputElement)} input 可选参数，用来指定一个input输入框，设定之后，在input输入文字将自动生成下拉选择列表。支持传入输入框DOM对象的id值，或直接传入输入框的DOM对象。
+   * @param {(String|HTMLDivElement)} output 可选参数，指定一个现有的div的id或者元素，作为展示提示结果的容器，当指定了input的时候有效，缺省的时候将自动创建一个显示结果面板
+   * @param {String} [type] 输入提示时限定POI类型，多个类型用“|”分隔，POI相关类型请在网站“相关下载”处下载目前只支持Poi类型编码如“050000”默认值：所有类别
+   * @param {String} [city] 输入提示时限定城市。可选值：城市名（中文或中文全拼）、citycode、adcode；默认值：“全国”
+   * @param {Boolean} [outPutDirAuto] 默认为true，表示是否在input位于页面较下方的时候自动将输入面板显示在input上方以避免被遮挡
+   * @memberof AutocompleteOptions
+   */
+  constructor(
+    datatype:String,
+
+    input:String|HTMLInputElement,
+
+    output:String|HTMLDivElement,
+
+    citylimit?:Boolean,
+
+    type?:String,
+    
+    city?:String,
+
+    outPutDirAuto?:Boolean
+  )
 }

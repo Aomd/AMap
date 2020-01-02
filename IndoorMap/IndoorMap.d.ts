43fb611060651334c00c5355616ed5b8d899f98c
Aomd <g00665@163.com>
2020-01-02 20:08:04 +0800

    更改文件大小写

---
+++ IndoorMap/IndoorMap.d.ts
@@ -0,0 +1,17 @@
+import { IndoorMapOptions } from './IndoorMapOptions'
+/**
+ *构造函数，实例化一个室内地图类
+ *
+ * @export
+ * @class IndoorMap
+ */
+export class IndoorMap {
+  /**
+   *Creates an instance of IndoorMap.
+   * @param {IndoorMapOptions} opts
+   * @memberof IndoorMap
+   */
+  constructor(
+    opts: Object | IndoorMapOptions,
+  )
+}

2973da187c6086ebc87f660ac8a16078bd4739b1
Aomd <g00665@163.com>
2020-01-02 20:27:01 +0800

    完善 ArrivalRange

--- ArrivalRange/ArrivalRange.d.ts
+++ ArrivalRange/ArrivalRange.d.ts
@@ -1,3 +1,23 @@
+import { LngLat } from "../LngLat";
+import {ArrivalRangeOptions} from './ArrivalRangeOptions';
+
+
+/**
+ * 根据输入的起点坐标和设定的时间范围
+ *
+ * @export
+ * @class ArrivalRange
+ */
 export class ArrivalRange{
-  
+
+  /**
+   * 计算某个时间段内用户通过公交出行可到达的距离范围
+   *
+   * @param {LngLat} origin
+   * @param {Number} time
+   * @param {Function} callback
+   * @param {ArrivalRangeOptions} opts
+   * @memberof ArrivalRange
+   */
+  search(origin:LngLat,time:Number,callback:Function,opts:ArrivalRangeOptions)
 }
--- ArrivalRange/ArrivalRangeOptions.d.ts
+++ ArrivalRange/ArrivalRangeOptions.d.ts
@@ -1,3 +1,25 @@
-export class ArrivalRangeOptions{
-  
+import { LngLat } from '../LngLat'
+
+
+
+/**
+ * 构造函数，示例化一个到达圈函数类
+ *
+ * @export
+ * @class ArrivalRangeOptions
+ */
+export class ArrivalRangeOptions {
+
+    /**
+     *Creates an instance of ArrivalRangeOptions.
+     * @param {String} policy 公交出行策略,可选为：只坐地铁：SUBWAY, 只坐公交：BUS，缺省时为公交+地铁,BUS,SUBWAY
+     * @param {String} resultType 结果返回样式：polygon：返回多边形边界值，coverage：判断设定的终点坐标是否在到达圈范围内
+     * @param {(LngLat | Array<LngLat>)} destination 选择一个想到达的目的地坐标，最多支持5个目的地坐标
+     * @memberof ArrivalRangeOptions
+     */
+    constructor(
+        policy: String,
+        resultType: String,
+        destination: LngLat | Array<LngLat>,
+    )
 }

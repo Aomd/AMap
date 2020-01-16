091a0d5e70b355d6455de9b173636fbc445c1d34
Aomd <g00665@163.com>
2020-01-16 20:34:50 +0800

    完善  BezierCurve

--- BezierCurve/BezierCurve.d.ts
+++ BezierCurve/BezierCurve.d.ts
@@ -1,3 +1,115 @@
-export class BezierCurve{
-  
+import { BezierCurveOptions } from './BezierCurveOptions'
+import { LngLat } from '../LngLat';
+import { PolylineOptions } from '../Polyline/PolylineOptions';
+import { Bounds } from '../Bounds';
+import { Map } from '../Map/Map';
+
+
+/**
+ * 贝瑟尔曲线类
+ *
+ * @export
+ * @class BezierCurve
+ */
+export class BezierCurve {
+  constructor(
+    opts: BezierCurveOptions | Object
+  )
+
+
+  /**
+   * 设置组成该折线的节点数组
+   *
+   * @param {Array<Array<LngLat>>} path
+   * @memberof BezierCurve
+   */
+  setPath(path: Array<Array<LngLat>>)
+
+
+  /**
+   * 获取折线路径的节点数组。其中lat和lng是经纬度参数。
+   *
+   * @returns {Array<Array<LngLat>>}
+   * @memberof BezierCurve
+   */
+  getPath(): Array<Array<LngLat>>
+
+
+  /**
+   * 修改折线属性（包括路径的节点、线样式、是否绘制大地线等。属性详情参看PolylineOptions列表）
+   *
+   * @param {PolylineOptions} opt
+   * @memberof BezierCurve
+   */
+  setOptions(opt: PolylineOptions)
+
+
+  /**
+   * 获取线的属性
+   *
+   * @returns {Object}
+   * @memberof BezierCurve
+   */
+  getOptions(): Object
+
+
+  /**
+   * 获取折线的总长度（单位：米）
+   *
+   * @returns {Number}
+   * @memberof BezierCurve
+   */
+  getLength(): Number
+
+
+  /**
+   * 获取当前折线的矩形范围对象
+   *
+   * @returns {Bounds}
+   * @memberof BezierCurve
+   */
+  getBounds(): Bounds
+
+
+  /**
+   * 地图上隐藏指定折线
+   *
+   * @memberof BezierCurve
+   */
+  hide()
+
+
+  /**
+   * 地图上显示指定折线
+   *
+   * @memberof BezierCurve
+   */
+  show()
+
+
+  /**
+   * 设置折线所在的地图。参数map即为目标地图，参数为null时，在地图上移除当前折线
+   *
+   * @param {Map} map
+   * @memberof BezierCurve
+   */
+  setMap(map: Map)
+
+
+  /**
+   *
+   * 设置用户自定义属性，支持JavaScript API任意数据类型，如Polyline的id等
+   * @param {*} ext
+   * @memberof BezierCurve
+   */
+  setExtData(ext: any)
+
+
+  /**
+   * 获取用户自定义属性
+   *
+   * @returns {*}
+   * @memberof BezierCurve
+   */
+  getExtData(): any
 }
--- BezierCurve/BezierCurveOptions.d.ts
+++ BezierCurve/BezierCurveOptions.d.ts
@@ -1,3 +1,61 @@
-export class BezierCurveOptions{
+import { LngLat } from '../LngLat';
+import { Map } from "../Map/Map";
+
+
+/**
+ * 用于创建曲线，ops为初始化构造参数
+ *
+ * @export
+ * @class BezierCurveOptions
+ */
+export class BezierCurveOptions {
   
+  /**
+   *Creates an instance of BezierCurveOptions.
+   * @param {Array<Array<LngLat>>} path 贝瑟尔曲线的路径。描述为一个二维数组规则
+   * @param {Map} map 所属地图
+   * @param {String} strokeColor 线条颜色，如‘#000000’、‘red’
+   * @param {Number} strokeOpacity 透明度
+   * @param {Number} strokeWeight 线宽
+   * @param {String} strokeStyle 虚线或者视线，'dashed'、'solid'
+   * @param {Array<Number>} strokeDasharray 虚线的分段，如[10,10]
+   * @param {Number} zIndex 层级
+   * @param {Boolean} showDir 是否显示白色方向箭头
+   * @param {Boolean} bubble 事件是否穿透到地图
+   * @param {String} cursor 指定鼠标悬停时的鼠标样式，自定义cursor，IE仅支持cur/ani/ico格式，Opera不支持自定义cursor
+   * @param {Boolean} isOutline 是否描边
+   * @param {String} outlineColor 描边颜色
+   * @param {Number} borderWeight 描边宽度
+   * @memberof BezierCurveOptions
+   */
+  constructor(
+
+    path: Array<Array<LngLat>>,
+
+    map: Map,
+
+    strokeColor: String,
+
+    strokeOpacity: Number,
+
+    strokeWeight: Number,
+
+    strokeStyle: String,
+
+    strokeDasharray: Array<Number>,
+
+    zIndex: Number,
+
+    showDir: Boolean,
+
+    bubble: Boolean,
+
+    cursor: String,
+
+    isOutline: Boolean,
+
+    outlineColor: String,
+
+    borderWeight: Number
+  )
 }

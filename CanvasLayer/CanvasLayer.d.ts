b1d96a476f2d4e265a038c2d6ec4afd2be0000f7
Aomd <g00665@163.com>
2020-01-16 20:54:54 +0800

    添加 CanvasLayer 注释

--- CanvasLayer/CanvasLayer.d.ts
+++ CanvasLayer/CanvasLayer.d.ts
@@ -14,22 +14,82 @@ export class CanvasLayer {
     opts: CanvasLayerOptions | Object
   )
 
+
+  /**
+   * 当canvas的内容发生改变是用于刷新图层，3D视图下调用，2D视图不需要调用
+   *
+   * @memberof CanvasLayer
+   */
   reFresh()
 
+
+  /**
+   * 设置图层所属的地图对象，传入null时从当前地图移除
+   *
+   * @param {(Map | null)} map
+   * @memberof CanvasLayer
+   */
   setMap(map: Map | null)
 
+
+  /**
+   * 返回图层所属的地图对象
+   *
+   * @returns {Map}
+   * @memberof CanvasLayer
+   */
   getMap(): Map
 
+
+  /**
+   * 显示
+   *
+   * @memberof CanvasLayer
+   */
   show()
 
+
+  /**
+   * 隐藏
+   *
+   * @memberof CanvasLayer
+   */
   hide()
 
+
+  /**
+   * 设置层级
+   *
+   * @param {Number} zindex
+   * @memberof CanvasLayer
+   */
   setzIndex(zindex: Number)
 
+
+  /**
+   * 获取层级
+   *
+   * @returns {Number}
+   * @memberof CanvasLayer
+   */
   getzIndex(): Number
 
+
+  /**
+   * 返回Canvas对象
+   *
+   * @returns {HTMLCanvasElement}
+   * @memberof CanvasLayer
+   */
   getElement(): HTMLCanvasElement
 
+
+  /**
+   * 修改显示的Canvas
+   *
+   * @param {HTMLCanvasElement} coxt
+   * @memberof CanvasLayer
+   */
   setCanvas(coxt: HTMLCanvasElement)
 
 
--- CanvasLayer/CanvasLayerOptions.d.ts
+++ CanvasLayer/CanvasLayerOptions.d.ts
@@ -1,7 +1,27 @@
 import { Bounds } from "../Bounds";
 import { Map } from "../Map/Map";
 
+
+/**
+ * Canas图层类，用户可以将一个Canvas作为图层添加在地图上，Canvas图层会随缩放级别而自适应缩放
+ *
+ * @export
+ * @class CanvasLayerOptions
+ */
 export class CanvasLayerOptions{
+
+
+  /**
+   *Creates an instance of CanvasLayerOptions.
+   * @param {Bounds} bounds VideoLayer覆盖的范围
+   * @param {HTMLCanvasElement} canvas 需要显示的Canvas对象
+   * @param {Number} opacity 图层的透明度，[0,1]
+   * @param {Boolean} visible 是否显示
+   * @param {Map} map 图层所属的地图对象
+   * @param {Number} zIndex 层级，缺省为12
+   * @param {[Number, Number]} zooms 设置可见级别，[最小级别，最大级别]
+   * @memberof CanvasLayerOptions
+   */
   constructor(
 
     bounds: Bounds,

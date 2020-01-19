eab863432e6e40585c41a66eb264fbe1d70fc065
Aomd <g00665@163.com>
2020-01-19 21:07:27 +0800

    完善 CircleMarker

--- CircleMarker/CircleMarker.d.ts
+++ CircleMarker/CircleMarker.d.ts
@@ -1,3 +1,138 @@
-export class CircleMarker{
-  
+import { CircleMarkerOptions } from './CircleMarkerOptions';
+import { LngLat } from '../LngLat';
+import { Map } from '../Map/Map';
+
+type CircleMarkerEventNames =
+  'click' |
+  'dblclick' |
+  'rightclick' |
+  'hide' |
+  'show' |
+  'mousedown' |
+  'mousedown' |
+  'mouseover' |
+  'mouseout' |
+  'change' |
+  'touchstart' |
+  'touchmove' |
+  'touchend';
+
+
+/**
+ *
+ *
+ * @export
+ * @class CircleMarker
+ */
+export class CircleMarker {
+
+
+  /**
+   *Creates an instance of CircleMarker.
+   * @param {(CircleMarkerOptions | Object)} opt 构造圆点标记，通过CircleMarkerOptions设置圆点标记的属性
+   * @memberof CircleMarker
+   */
+  constructor(
+    opt: CircleMarkerOptions | Object
+  )
+
+
+  /**
+   * 设置圆点中心
+   *
+   * @param {LngLat} lnglat
+   * @memberof CircleMarker
+   */
+  setCenter(lnglat: LngLat)
+
+
+  /**
+   * 获取圆点中心
+   *
+   * @returns {LngLat}
+   * @memberof CircleMarker
+   */
+  getCenter(): LngLat
+
+
+  /**
+   * 设置圆点的半径
+   *
+   * @param {Number} radius
+   * @memberof CircleMarker
+   */
+  setRadius(radius: Number)
+
+
+  /**
+   * 获取圆点的半径
+   *
+   * @returns {Number}
+   * @memberof CircleMarker
+   */
+  getRadius(): Number
+
+
+
+  /**
+   * 修改圆点标记的属性（样式风格，包括轮廓线、填充色等。属性详情参看CircleMarkerOptions列表）
+   *
+   * @param {CircleMarkerOptions} circleopt
+   * @memberof CircleMarker
+   */
+  setOptions(circleopt: CircleMarkerOptions)
+
+
+  /**
+   * 获取圆点的属性
+   *
+   * @returns {Object}
+   * @memberof CircleMarker
+   */
+  getOptions(): Object
+
+
+  /**
+   * 地图上隐藏圆形
+   *
+   * @memberof CircleMarker
+   */
+  hide()
+
+
+  /**
+   * 地图上显示圆形
+   *
+   * @memberof CircleMarker
+   */
+  show()
+
+
+  /**
+   * 在指定地图上添加该圆点覆盖物。参数取值为null时，在地图上移除当前圆形
+   *
+   * @param {Map} map
+   * @memberof CircleMarker
+   */
+  setMap(map: Map)
+
+
+  /**
+   * 设置用户自定义属性，支持JavaScript API任意数据类型，如CircleMarker的id等
+   *
+   * @param {*} ext
+   * @memberof CircleMarker
+   */
+  setExtData(ext: any)
+
+
+  /**
+   * 获取用户自定义属性
+   *
+   * @returns {*}
+   * @memberof CircleMarker
+   */
+  getExtData(): any
+
+  on(event: CircleMarkerEventNames, cb: Function)
 }
--- CircleMarker/CircleMarkerOptions.d.ts
+++ CircleMarker/CircleMarkerOptions.d.ts
@@ -1,3 +1,45 @@
-export class CircleMarkerOptions{
-  
+import { Map } from "../Map/Map";
+import { LngLat } from "../LngLat";
+
+export class CircleMarkerOptions {
+
+  /**
+   *Creates an instance of CircleMarkerOptions.
+   * @param {Map} map 要显示该标记的地图对象
+   * @param {Number} zIndex 层叠顺序
+   * @param {LngLat} center 圆心位置
+   * @param {Boolean} bubble 是否将覆盖物的鼠标或touch等事件冒泡到地图上
+   * @param {Number} radius 圆点半径，单位:px
+   * @param {String} strokeColor 线条颜色，使用16进制颜色代码赋值。默认值为#006600
+   * @param {Number} strokeOpacity 轮廓线透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+   * @param {Number} strokeWeight 轮廓线宽度
+   * @param {String} fillColor 圆形填充颜色,使用16进制颜色代码赋值。默认值为#006600
+   * @param {Number} fillOpacity 圆形填充透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+   * @param {*} extData 用户自定义属性，支持JavaScript API任意数据类型，如Circle的id等
+   * @memberof CircleMarkerOptions
+   */
+  constructor(
+    map: Map,
+
+    zIndex: Number,
+
+    center: LngLat,
+
+    bubble: Boolean,
+
+    radius: Number,
+
+    strokeColor: String,
+
+    strokeOpacity: Number,
+
+    strokeWeight: Number,
+
+
+    fillColor: String,
+
+    fillOpacity: Number,
+
+    extData: any
+  )
 }

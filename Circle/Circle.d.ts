ae7837f0eeaf7b325c484c0f931c97638885af02
Aomd <g00665@163.com>
2020-01-19 21:07:08 +0800

    完善  Circle

--- Circle/Circle.d.ts
+++ Circle/Circle.d.ts
@@ -1,3 +1,149 @@
-export class Circle{
-  
+import { CircleOptions } from './CircleOptions.d';
+import { LngLat } from '../LngLat';
+import { Bounds } from '../Bounds';
+import { Map } from '../Map/Map';
+
+type CircleEventNames =
+  'click' |
+  'dblclick' |
+  'rightclick' |
+  'hide' |
+  'show' |
+  'mousedown' |
+  'mouseup' |
+  'mouseover' |
+  'mouseout' |
+  'change' |
+  'touchstart' |
+  'touchmove' |
+  'touchend' |
+  'end';
+
+
+/**
+ * Circle类
+ *
+ * @export
+ * @class Circle
+ */
+export class Circle {
+  constructor(opt: CircleOptions | Object)
+
+
+  /**
+   * 设置圆中心点
+   *
+   * @param {LngLat} lnglat
+   * @memberof Circle
+   */
+  setCenter(lnglat: LngLat)
+
+
+  /**
+   *
+   * 获取圆中心点
+   * @returns {LngLat}
+   * @memberof Circle
+   */
+  getCenter(): LngLat
+
+
+  /**
+   * 获取圆外切矩形范围
+   *
+   * @returns {Bounds}
+   * @memberof Circle
+   */
+  getBounds(): Bounds
+
+
+  /**
+   * 设置圆形的半径
+   *
+   * @param {Number} radius
+   * @memberof Circle
+   */
+  setRadius(radius: Number)
+
+
+  /**
+   * 获取圆形的半径
+   *
+   * @returns {Number}
+   * @memberof Circle
+   */
+  getRadius(): Number
+
+
+  /**
+   * 修改圆属性（样式风格，包括组成圆形轮廓线的节点、轮廓线样式等。属性详情参看CircleOptions列表）
+   *
+   * @param {CircleOptions} circleopt
+   * @memberof Circle
+   */
+  setOptions(circleopt: CircleOptions)
+
+
+  /**
+   * 获取圆形的属性
+   *
+   * @returns {Object}
+   * @memberof Circle
+   */
+  getOptions(): Object
+
+
+  /**
+   * 地图上隐藏圆形
+   *
+   * @memberof Circle
+   */
+  hide()
+
+
+  /**
+   * 地图上显示圆形
+   *
+   * @memberof Circle
+   */
+  show()
+
+
+  /**
+   * 在指定地图上添加该圆形覆盖物。参数取值为null时，在地图上移除当前圆形
+   *
+   * @param {Map} map
+   * @memberof Circle
+   */
+  setMap(map: Map)
+
+
+  /**
+   * 设置用户自定义属性，支持JavaScript API任意数据类型，如Circle的id等
+   *
+   * @param {*} ext
+   * @memberof Circle
+   */
+  setExtData(ext: any)
+
+
+  /**
+   * 获取用户自定义属性
+   *
+   * @returns {*}
+   * @memberof Circle
+   */
+  getExtData(): any
+
+
+  /**
+   * 判断指定点坐标是否在圆内
+   *
+   * @param {LngLat} point
+   * @returns {Boolean}
+   * @memberof Circle
+   */
+  contains(point: LngLat): Boolean
+
+  on(event: CircleEventNames, cb: Function)
 }
--- Circle/CircleOptions.d.ts
+++ Circle/CircleOptions.d.ts
@@ -1,3 +1,47 @@
-export class CircleOptions{
-  
+import { Map } from '../Map/Map'
+import { LngLat } from '../LngLat';
+
+
+/**
+ * 构造圆形覆盖物，通过CircleOptions设置圆形
+ *
+ * @export
+ * @class CircleOptions
+ */
+export class CircleOptions {
+
+    /**
+     *Creates an instance of CircleOptions.
+     * @param {Map} map 要显示该circle的地图对象
+     * @param {Number} zIndex 层叠顺序默认zIndex:10
+     * @param {LngLat} center 圆心位置
+     * @param {Boolean} bubble 是否将覆盖物的鼠标或touch等事件冒泡到地图上
+     * @param {String} Boolean 指定鼠标悬停时的鼠标样式，自定义cursor，IE仅支持cur/ani/ico格式，Opera不支持自定义cursor
+     * @param {Number} radius 圆半径，单位:米
+     * @param {String} strokeColor 线条颜色，使用16进制颜色代码赋值。默认值为#006600
+     * @param {Number} strokeOpacity 轮廓线透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+     * @param {Number} strokeWeight 轮廓线宽度
+     * @param {String} fillColor 圆形填充颜色,使用16进制颜色代码赋值。默认值为#006600
+     * @param {Number} fillOpacity 圆形填充透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+     * @param {String} strokeStyle 轮廓线样式，实线:solid，虚线:dashed
+     * @param {*} extData 用户自定义属性，支持JavaScript API任意数据类型，如Circle的id等
+     * @param {Array<Number>} strokeDasharray 勾勒形状轮廓的虚线和间隙的样式，此属性在strokeStyle 为dashed 时有效， 此属性在ie9+浏览器有效
+     * @memberof CircleOptions
+     */
+    constructor(
+        map: Map,
+        zIndex:Number,
+        center:LngLat,
+        bubble:Boolean,
+        Boolean:String,
+        radius:Number,
+        strokeColor:String,
+        strokeOpacity:Number,
+        strokeWeight:Number,
+        fillColor:String,
+        fillOpacity:Number,
+        strokeStyle:String,
+        extData:any,
+        strokeDasharray:Array<Number>
+    )
 }

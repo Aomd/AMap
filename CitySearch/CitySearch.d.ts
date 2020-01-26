ae7857f3b26b3006c16e8ae6dea1d51c9b472549
Aomd <g00665@163.com>
2020-01-26 23:20:24 +0800

    完善 CitySearch

--- CitySearch/CitySearch.d.ts
+++ CitySearch/CitySearch.d.ts
@@ -1,3 +1,33 @@
-export class CitySearch{
-  
+import { CitySearchResult } from './CitySearchResult';
+
+type CitySearchEventNames =
+  'complete' |
+  'error';
+
+export class CitySearch {
+
+
+  /**
+   * 自动获取用户IP，回调返回当前用户所在城市
+   * 当status为complete时，result为CitySearchResult；
+   * 当status为error时，result为错误信息info；
+   * 当status为no_data时，代表检索返回0结果
+   *
+   * @param {((status: String, result: Object | CitySearchResult) => void)} cbk
+   * @memberof CitySearch
+   */
+  getLocalCity(cbk: (status: String, result: Object | CitySearchResult) => void)
+
+
+  /**
+   * 根据输入IP地址返回对应城市信息
+   * status同上
+   *
+   * @param {String} ip
+   * @param {((status: String, result: Object | CitySearchResult) => void)} callback
+   * @memberof CitySearch
+   */
+  getCityByIp(ip: String, callback: (status: String, result: Object | CitySearchResult) => void)
+
+  on(event: CitySearchEventNames, cb: Function)
 }
---
+++ CitySearch/CitySearchResult.d.ts
@@ -0,0 +1,15 @@
+import { Bounds } from "../Bounds";
+
+export class CitySearchResult {
+
+  /**
+   *Creates an instance of CitySearchResult.
+   * @param {String} city 城市名称
+   * @param {Bounds} bounds 地图展示该城市时使用的矩形区域
+   * @memberof CitySearchResult
+   */
+  constructor(
+    city: String,
+    bounds: Bounds
+  )
+}

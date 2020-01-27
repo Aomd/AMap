05247306141d428cd50bacc6dfe4b5aa7a7473b5
Aomd <g00665@163.com>
2020-01-27 19:15:03 +0800

    完善  DistrictSearch

--- DistrictSearch/DistrictSearch.d.ts
+++ DistrictSearch/DistrictSearch.d.ts
@@ -1,3 +1,14 @@
+import {DistrictSearchOptions} from './DistrictSearchOptions';
+
+
+/**
+ * 构造函数，实例化一个行政区查询实例
+ *
+ * @export
+ * @class DistrictSearch
+ */
 export class DistrictSearch{
-  
+  constructor(
+    opts:Object| DistrictSearchOptions
+  )
 }
--- DistrictSearch/DistrictSearchOptions.d.ts
+++ DistrictSearch/DistrictSearchOptions.d.ts
@@ -1,3 +1,18 @@
-export class DistrictSearchOptions{
-  
+export class DistrictSearchOptions {
+
+
+  /**
+   *Creates an instance of DistrictSearchOptions.
+   * @param {String} level 关键字对应的行政区级别或商圈
+   * @param {Boolean} showbiz 是否显示商圈，默认值true
+   * @param {String} extensions 是否返回行政区边界坐标点
+   * @param {Number} subdistrict 显示下级行政区级数
+   * @memberof DistrictSearchOptions
+   */
+  constructor(
+    level: String,
+    showbiz: Boolean,
+    extensions: String,
+    subdistrict: Number
+  )
 }

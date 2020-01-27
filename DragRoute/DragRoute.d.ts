ad227374e7e9b4c4a9716c377a53d4772b4792af
Aomd <g00665@163.com>
2020-01-27 19:15:15 +0800

    完善 DragRoute

--- DragRoute/DragRoute.d.ts
+++ DragRoute/DragRoute.d.ts
@@ -1,3 +1,57 @@
-export class DragRoute{
-  
+import { Map } from "../Map/Map";
+import { LngLat } from "../LngLat";
+import { DragRouteOptions } from './DragRouteOptions'
+
+type DrivingPolicy =
+  'LEAST_TIME' |
+  'LEAST_FEE' |
+  'LEAST_DISTANCE' |
+  'REAL_TRAFFIC';
+
+  type DragRouteEventNames = 
+  'addway'|
+  'complete';
+
+export class DragRoute {
+  constructor(
+    map: Map, path: Array<LngLat>, policy: DrivingPolicy, opts: DragRouteOptions |Object
+  )
+
+
+  /**
+   * 开始路径导航。支持鼠标拖拽导航路径节点，更改途经点时，系统实时重新计算并显示导航路径
+   *
+   * @memberof DragRoute
+   */
+  search()
+
+
+  /**
+   * 返回除了起点和终点之外的所有点
+   *
+   * @returns {Array<LngLat>}
+   * @memberof DragRoute
+   */
+  getWays(): Array<LngLat>
+
+
+  /**
+   * 返回当前导航路径，即导航路径的经纬度坐标数组，需要在complete回调中执行。
+   *
+   * @returns {Array<LngLat>}
+   * @memberof DragRoute
+   */
+  getRoute(): Array<LngLat>
+
+
+  /**
+   * 销毁拖拽导航插件。
+   *
+   * @memberof DragRoute
+   */
+  destroy()
+
+
+  on(event: DragRouteEventNames, cb: Function)
+
 }
--- DragRoute/DragRouteOptions.d.ts
+++ DragRoute/DragRouteOptions.d.ts
@@ -1,3 +1,21 @@
 export class DragRouteOptions{
-  
+
+
+    /**
+     *Creates an instance of DragRouteOptions.
+     * (详情)[https://lbs.amap.com/api/javascript-api/reference/route-search/?sug_index=1#AMap.DragRoute]
+     * @param {Object} polyOptions 设置拖拽路线插件的路线属性对象，包括线样式、宽度、颜色等，参考PolylineOptions列表。
+     * @param {Object} startMarkerOptions 设置拖拽路线插件起点点标记属性对象，包括点标记样式、大小等，参考MarkerOptions列表。
+     * @param {Object} midMarkerOptions 设置拖拽路线插件途经点点标记属性对象，包括点标记样式、大小等，参考MarkerOptions列表列表。
+     * @param {Object} endMarkerOptions 设置拖拽路线插件终点点标记属性对象，包括点标记样式、大小等，参考MarkerOptions列表列表。
+     * @param {Boolean} showTraffic 设置显示实时路况信息，true：路线中显示实时路况信息，false：关闭实时路况信息默认值：true
+     * @memberof DragRouteOptions
+     */
+    constructor(
+        polyOptions:Object,
+        startMarkerOptions:Object,
+        midMarkerOptions:Object,
+        endMarkerOptions:Object,
+        showTraffic:Boolean
+    )
 }

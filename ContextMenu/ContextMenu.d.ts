a5f86aa191737fb3fbdd2c38824526250dd28a76
Aomd <g00665@163.com>
2020-01-27 19:14:15 +0800

    完善 ContextMenu

--- ContextMenu/ContextMenu.d.ts
+++ ContextMenu/ContextMenu.d.ts
@@ -1,3 +1,54 @@
-export class ContextMenu{
-  
+import { ContextMenuOptions } from './ContextMenuOptions';
+import { Map } from '../Map/Map';
+import { LngLat } from '../LngLat';
+
+type ContextMenuEventNames =
+  'open' |
+  'close';
+
+export class ContextMenu {
+  constructor(
+    opt: ContextMenuOptions
+  )
+
+
+  /**
+   *
+   *
+   * @param {String} text 菜单显示内容
+   * @param {Function} fn 该菜单下需进行的操作
+   * @param {Number} num 当前菜单项在右键菜单中的排序位置，以0开始
+   * @memberof ContextMenu
+   */
+  addItem(text: String, fn: Function, num: Number)
+
+
+  /**
+   * 删除一个菜单项
+   *
+   * @param {String} text  菜单显示内容
+   * @param {Function} fn 该菜单下需进行的操作
+   * @memberof ContextMenu
+   */
+  removeItem(text: String, fn: Function)
+
+
+  /**
+   * 在地图的指定位置打开右键菜单。
+   *
+   * @param {Map} map
+   * @param {LngLat} position
+   * @memberof ContextMenu
+   */
+  open(map: Map, position: LngLat)
+
+
+  /**
+   * 关闭右键菜单
+   *
+   * @memberof ContextMenu
+   */
+  close()
+
+  on(event: ContextMenuEventNames, cb: Function)
 }
--- ContextMenu/ContextMenuOptions.d.ts
+++ ContextMenu/ContextMenuOptions.d.ts
@@ -1,3 +1,17 @@
-export class ContextMenuOptions{
-  
+import { LngLat } from "../LngLat";
+
+export class ContextMenuOptions {
+
+  /**
+   *Creates an instance of ContextMenuOptions.
+   * @param {LngLat} position 右键菜单显示的位置
+   * @param {(String | HTMLElement)} content 右键菜单内容（针对自定义菜单时，添加菜单内容及功能。可以是HTML要素字符串或者HTML DOM对象。）
+   * @param {Number} width 右键菜单宽度
+   * @memberof ContextMenuOptions
+   */
+  constructor(
+    position: LngLat,
+    content: String | HTMLElement,
+    width: Number
+  )
 }

f5ca5784c33725d5d5c00bef3505a528e6d1f9c1
Aomd <g00665@163.com>
2020-01-27 19:14:49 +0800

    完善 CustomLayer

--- CustomLayer/CustomLayer.d.ts
+++ CustomLayer/CustomLayer.d.ts
@@ -6,27 +6,67 @@ export class CustomLayer {
     ops: CustomLayerOptions | Object
   )
 
+
+  /**
+   * 设置透明度
+   *
+   * @memberof CustomLayer
+   */
   setOpacity()
 
 
 
 
+  /**
+   * 设置图层所属的地图对象，传入null时从当前地图移除
+   *
+   * @param {(Map | null)} map
+   * @memberof CustomLayer
+   */
   setMap(map: Map | null)
 
 
+  /**
+   * 返回图层所属的地图对象
+   *
+   * @returns {Map}
+   * @memberof CustomLayer
+   */
   getMap(): Map
 
 
+  /**
+   * 显示
+   *
+   * @memberof CustomLayer
+   */
   show()
 
 
+  /**
+   * 隐藏
+   *
+   * @memberof CustomLayer
+   */
   hide()
 
 
+  /**
+   * 设置层级
+   *
+   * @param {Number} zindex
+   * @memberof CustomLayer
+   */
   setzIndex(zindex: Number)
 
 
 
+  /**
+   * 获取层级
+   *
+   * @returns {Number}
+   * @memberof CustomLayer
+   */
   getzIndex(): Number
 
 }
--- CustomLayer/CustomLayerOptions.d.ts
+++ CustomLayer/CustomLayerOptions.d.ts
@@ -1,6 +1,15 @@
 import { Map } from "../Map/Map";
 
 export class CustomLayerOptions {
+
+  /**
+   *Creates an instance of CustomLayerOptions.
+   * @param {Map} map 所属的地图对象
+   * @param {Number} zIndex 层级
+   * @param {Number} opacity 透明度
+   * @param {[Number, Number]} zooms 显示级别
+   * @memberof CustomLayerOptions
+   */
   constructor(
     map: Map,
 

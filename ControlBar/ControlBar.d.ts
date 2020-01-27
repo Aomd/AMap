a2274ccb6ca5283365a39dc4a4ce1ea559938e12
Aomd <g00665@163.com>
2020-01-27 19:14:28 +0800

    完善 ControlBar

--- ControlBar/ControlBar.d.ts
+++ ControlBar/ControlBar.d.ts
@@ -1,3 +1,20 @@
-export class ControlBar{
-  
+import { ControlBarOptions } from './ControlBarOptions';
+
+
+/**
+ * 组合了旋转、倾斜、复位、缩放在内的地图控件，自V1.4.0版本新增。
+ *
+ * @export
+ * @class ControlBar
+ */
+export class ControlBar {
+
+  /**
+   *Creates an instance of ControlBar.
+   * @param {(Object|ControlBarOptions)} opt
+   * @memberof ControlBar
+   */
+  constructor(
+    opt: Object | ControlBarOptions
+  )
 }
--- ControlBar/ControlBarOptions.d.ts
+++ ControlBar/ControlBarOptions.d.ts
@@ -1,3 +1,15 @@
 export class ControlBarOptions{
-  
+
+  /**
+   *Creates an instance of ControlBarOptions.
+   * @param {Object} position 控制显示位置，如{top:’10px’,right:’10px’}
+   * @param {Boolean} showZoomBar 是否显示缩放按钮。移动端默认为false，PC端为默认为true
+   * @param {Boolean} showControlButton 是否显示倾斜、旋转按钮。移动端默认为false，PC端为默认为true
+   * @memberof ControlBarOptions
+   */
+  constructor(
+    position:Object,
+    showZoomBar:Boolean,
+    showControlButton:Boolean
+  )
 }

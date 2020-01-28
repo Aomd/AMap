b8dffb9fefbded0ae824548fbf6c3edb5659a673
Aomd <g00665@163.com>
2020-01-28 22:09:22 +0800

    完善  Driving

--- Driving/Driving.d.ts
+++ Driving/Driving.d.ts
@@ -1,3 +1,115 @@
-export class Driving{
-  
+import { DrivingOptions } from './DrivingOptions';
+import { LngLat } from '../LngLat';
+import { DrivingPolicy } from '../DragRoute/DragRoute';
+
+type DrivingEventNames = 
+'complete'|
+'error';
+
+export class Driving {
+  constructor(
+    opts: DrivingOptions | Object
+  )
+
+
+  /**
+   * 根据起点、终点和途经点（可选）坐标或名称，实现驾车路线规划，途经点通过opts设定，最多支持16个途径点
+   *
+   * @param {LngLat} origin
+   * @param {LngLat} destination
+   * @param {Object} opts
+   * @param {(status: String, result: Object) => void} callback
+   * @memberof Driving
+   */
+  search(origin: LngLat, destination: LngLat, opts: Object, callback: (status: String, result: Object) => void)
+
+  // search(points: Array<Object>, callback: (status: String, result: Object) => void)
+
+
+  /**
+   * 设置驾车路线规划策略
+   *
+   * @param {DrivingPolicy} policy
+   * @memberof Driving
+   */
+  setPolicy(policy:DrivingPolicy)
+
+
+  /**
+   * 设置避让区域，最大支持三个避让区域，参数为LngLat的二维数组
+   *
+   * @param {Array<Array<LngLat>>} path
+   * @memberof Driving
+   */
+  setAvoidPolygons(path:Array<Array<LngLat>>)
+
+
+  /**
+   * 设置避让道路名称，只支持一条避让道路                    注：避让道路和避让区域不能同时使用
+   *
+   * @param {String} road
+   * @memberof Driving
+   */
+  setAvoidRoad(road:String)
+
+
+  /**
+   * 清除避让道路
+   *
+   * @memberof Driving
+   */
+  clearAvoidRoad()
+
+
+  /**
+   * 清除避让区域
+   *
+   * @memberof Driving
+   */
+  clearAvoidPolygons()
+
+
+  /**
+   * 获取避让区域，返回LngLat的二维数组
+   *
+   * @memberof Driving
+   */
+  getAvoidPolygons() :Array<LngLat>
+
+
+  /**
+   * 获取避让道路
+   *
+   * @memberof Driving
+   */
+  getAvoidRoad():String
+
+
+  /**
+   * 清除搜索结果
+   *
+   * @memberof Driving
+   */
+  clear()
+
+
+  /**
+   * 唤起高德地图客户端驾车路径规划
+   *
+   * @param {Object} obj
+   * @memberof Driving
+   */
+  searchOnAMAP(obj:Object)
+
+
+  /**
+   * 设置车牌的汉字首字符和首字后的号码，设置后路线规划的结果将考虑该车牌在当前时间的限行路段
+   *
+   * @param {String} province
+   * @param {String} number
+   * @memberof Driving
+   */
+  setProvinceAndNumber(province:String,number:String)
+
+  on(event: DrivingEventNames, cb: Function)
 }
--- Driving/DrivingOptions.d.ts
+++ Driving/DrivingOptions.d.ts
@@ -1,3 +1,43 @@
+import { DrivingPolicy } from "../DragRoute/DragRoute";
+import { Map } from "../Map/Map";
+
+
+/**
+ *
+ *
+ * @export
+ * @class DrivingOptions
+ */
 export class DrivingOptions{
-  
+
+  /**
+   *Creates an instance of DrivingOptions.
+   * @param {DrivingPolicy} policy 驾车路线规划策略
+   * @param {String} extensions 默认值：base，返回基本地址信息当取值为：all，返回DriveStep基本信息+DriveStep详细信息
+   * @param {Number} ferry 默认为0，表示可以使用轮渡，为1的时候表示不可以使用轮渡
+   * @param {Map} map AMap.Map对象, 展现结果的地图实例。当指定此参数后，搜索结果的标注、线路等均会自动添加到此地图上。可选
+   * @param {(String|HTMLElement)} panel 结果列表的HTML容器id或容器元素，提供此参数后，结果列表将在此容器中进行展示。可选
+   * @param {Boolean} hideMarkers 设置隐藏路径规划的起始点图标，设置为true：隐藏图标；设置false：显示图标默认值为：false
+   * @param {Boolean} showTraffic 设置是否显示实时路况信息，默认设置为true。
+   * @param {String} province 车牌省份的汉字缩写，用于判断是否限行，与number属性组合使用，可选。例如：京
+   * @param {String} number 除省份之外车牌的字母和数字，用于判断限行相关，与province属性组合使用，可选。例如:NH1N11
+   * @param {Boolean} isOutline 使用map属性时，绘制的规划线路是否显示描边。缺省为true
+   * @param {String} outlineColor 使用map属性时，绘制的规划线路的描边颜色。缺省为'white'
+   * @param {Boolean} autoFitView 用于控制在路径规划结束后，是否自动调整地图视野使绘制的路线处于视口的可见范围
+   * @memberof DrivingOptions
+   */
+  constructor(
+    policy:DrivingPolicy,
+    extensions:String,
+    ferry:Number,
+    map:Map,
+    panel:String|HTMLElement,
+    hideMarkers:Boolean,
+    showTraffic:Boolean,
+    province:String,
+    number:String,
+    isOutline:Boolean,
+    outlineColor:String,
+    autoFitView:Boolean,
+  )
 }

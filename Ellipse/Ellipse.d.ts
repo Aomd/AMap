d7ae1c9a812686562443ce416adc6dfa8c910d4d
Aomd <g00665@163.com>
2020-01-28 22:09:40 +0800

    完善 Ellipse

--- Ellipse/Ellipse.d.ts
+++ Ellipse/Ellipse.d.ts
@@ -1,3 +1,125 @@
+import {EllipseOptions} from './EllipseOptions'
+import { LngLat } from '../LngLat';
+import { Bounds } from '../Bounds';
+import { Map } from '../Map/Map';
+
+type EllipseEventNames = 
+'click'|
+'dblclick'|
+'rightclick'|
+'hide'|
+'show'|
+'mousedown'|
+'mouseup'|
+'mouseover'|
+'mouseout'|
+'change'|
+'touchstart'|
+'touchmove'|
+'touchend';
+
 export class Ellipse{
-  
+  constructor(
+    opt:EllipseOptions|Object
+  )
+
+
+  /**
+   * 获取椭圆的中心点
+   *
+   * @returns {LngLat}
+   * @memberof Ellipse
+   */
+  getCenter():LngLat
+
+
+  /**
+   * 设置椭圆的中心点
+   *
+   * @param {LngLat} lnglat
+   * @memberof Ellipse
+   */
+  setCenter(lnglat: LngLat)
+
+
+  /**
+   * 获取椭圆范围
+   *
+   * @returns {Bounds}
+   * @memberof Ellipse
+   */
+  getBounds():Bounds
+
+
+  /** 
+   * 修改椭圆属性
+   *
+   * @param {EllipseOptions} ellipseOpt
+   * @memberof Ellipse
+   */
+  setOptions(ellipseOpt:EllipseOptions)
+
+
+  /**
+   * 获取椭圆的属性
+   *
+   * @returns {Object}
+   * @memberof Ellipse
+   */
+  getOptions():Object
+
+
+  /** 
+   * 地图上隐藏椭圆
+   *
+   * @memberof Ellipse
+   */
+  hide()
+
+
+  /**
+   * 地图上显示椭圆
+   *
+   * @memberof Ellipse
+   */
+  show()
+
+
+  /**
+   * 在指定地图上添加该椭圆覆盖物。参数取值为null时，在地图上移除当前椭圆
+   *
+   * @param {Map} map
+   * @memberof Ellipse
+   */
+  setMap(map:Map)
+
+
+  /**
+   * 设置用户自定义属性，支持JavaScript API任意数据类型，如Ellipse的id等
+   *
+   * @param {*} ext
+   * @memberof Ellipse
+   */
+  setExtData(ext:any)
+
+
+  /**
+   * 获取用户自定义属性
+   *
+   * @returns {*}
+   * @memberof Ellipse
+   */
+  getExtData():any
+
+
+  /**
+   * 判断指定点坐标是否在椭圆内
+   *
+   * @param {LngLat} point
+   * @returns {Boolean}
+   * @memberof Ellipse
+   */
+  contains(point:LngLat):Boolean
+
+  on(event: EllipseEventNames, cb: Function)
 }
--- Ellipse/EllipseOptions.d.ts
+++ Ellipse/EllipseOptions.d.ts
@@ -1,3 +1,40 @@
-export class EllipseOptions{
-  
+import { Map } from "../Map/Map";
+import { LngLat } from "../LngLat";
+
+export class EllipseOptions {
+
+    /**
+     *Creates an instance of EllipseOptions.
+     * @param {Map} map 要显示该ellipse的地图对象
+     * @param {Number} zIndex 层叠顺序默认zIndex:10
+     * @param {LngLat} center 椭圆的圆心位置
+     * @param {Array<Number>} radius 椭圆的半径，用2个元素的数组表示，单位：米
+     * @param {Boolean} bubble是否将覆盖物的鼠标或touch等事件冒泡到地图上默认值：false
+     * @param {String} cursor 指定鼠标悬停时的鼠标样式，自定义cursor，IE仅支持cur/ani/ico格式，Opera不支持自定义cursor
+     * @param {String} strokeColor 线条颜色，使用16进制颜色代码赋值。默认值为#006600
+     * @param {Number} strokeOpacity 轮廓线透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+     * @param {Number} strokeWeight 轮廓线宽度
+     * @param {String} fillColor 椭圆填充颜色,使用16进制颜色代码赋值。默认值为#006600
+     * @param {Number} fillOpacity 椭圆填充透明度，取值范围[0,1]，0表示完全透明，1表示不透明。默认为0.9
+     * @param {String} strokeStyle 轮廓线样式，实线:solid，虚线:dashed
+     * @param {*} extData 用户自定义属性，支持JavaScript API任意数据类型，如Ellipse的id等
+     * @param {Array<Number>} strokeDasharray 勾勒形状轮廓的虚线和间隙的样式，此属性在strokeStyle 为dashed 时有效， 此属性在ie9+浏览器有效 取值：
+     * @memberof EllipseOptions
+     */
+    constructor(
+        map: Map,
+        zIndex: Number,
+        center: LngLat,
+        radius: Array<Number>,
+        bubble: Boolean,
+        cursor: String,
+        strokeColor: String,
+        strokeOpacity: Number,
+        strokeWeight: Number,
+        fillColor: String,
+        fillOpacity: Number,
+        strokeStyle: String,
+        extData: any,
+        strokeDasharray: Array<Number>
+    )
 }

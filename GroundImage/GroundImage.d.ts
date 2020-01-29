bc856cb1d13c83a06d3f0b4dd67464382534a5be
Aomd <g00665@163.com>
2020-01-29 21:28:32 +0800

    完善 GroundImage

--- GroundImage/GroundImage.d.ts
+++ GroundImage/GroundImage.d.ts
@@ -1,3 +1,66 @@
-export class GroundImage{
-  
+import { Bounds } from "../Bounds";
+
+import { GroundImageOptions } from './GroundImageOptions'
+import { Map } from "../Map/Map";
+
+type GroundImageEventNames = 'click'|'dblclick';
+
+export class GroundImage {
+  constructor(url: String, bounds: Bounds, opt: GroundImageOptions | Object)
+
+
+  /**
+   * 获取GroundImage的Map对象
+   *
+   * @returns {Map}
+   * @memberof GroundImage
+   */
+  getMap(): Map
+
+
+  /**
+   * 设置显示GroundImage的Map对象
+   *
+   * @param {Map} map
+   * @memberof GroundImage
+   */
+  setMap(map: Map)
+
+
+  /**
+   * 获取图片透明度值
+   *
+   * @returns {Number}
+   * @memberof GroundImage
+   */
+  getOpacity(): Number
+
+
+  /**
+   * 设置图片透明度
+   *
+   * @param {Number} opacity
+   * @memberof GroundImage
+   */
+  setOpacity(opacity: Number)
+
+
+  /**
+   * 获取GroundImage的覆盖地理范围
+   *
+   * @returns {Bounds}
+   * @memberof GroundImage
+   */
+  getBounds(): Bounds
+
+
+  /**
+   * 获取图片url
+   *
+   * @returns {String}
+   * @memberof GroundImage
+   */
+  getImageUrl(): String
+
+  on(event: GroundImageEventNames, cb: Function)
 }
--- GroundImage/GroundImageOptions.d.ts
+++ GroundImage/GroundImageOptions.d.ts
@@ -1,3 +1,17 @@
+import { Map } from "../Map/Map";
+
 export class GroundImageOptions{
 
+    /**
+     *Creates an instance of GroundImageOptions.
+     * @param {Map} map 要显示该groundimage的地图对象
+     * @param {Boolean} clickable 图层是否可点击，若为可点击则GroundImage支持鼠标点击事件
+     * @param {Number} opacity 图片透明度，取值范围[0,1]，0表示完全透明，1表示不透明
+     * @memberof GroundImageOptions
+     */
+    constructor(
+        map:Map,
+        clickable:Boolean,
+        opacity:	Number
+    )
 }

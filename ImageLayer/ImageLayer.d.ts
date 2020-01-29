4226de0c2ed3f13be475a486b01c400f0a01f3f6
Aomd <g00665@163.com>
2020-01-29 21:29:01 +0800

    完善 ImageLayer

--- ImageLayer/ImageLayer.d.ts
+++ ImageLayer/ImageLayer.d.ts
@@ -7,26 +7,101 @@ export class ImageLayer {
     opts: Object | ImageLayerOptions
   )
 
+
+  /**
+   * 设置图层所属的地图对象，传入null时从当前地图移除
+   *
+   * @param {(Map | null)} map
+   * @memberof ImageLayer
+   */
   setMap(map: Map | null)
 
+
+  /**
+   * 返回图层所属的地图对象
+   *
+   * @returns {Map}
+   * @memberof ImageLayer
+   */
   getMap(): Map
 
+
+  /**
+   * 返回ImageLayer显示的范围
+   *
+   * @returns {Bounds}
+   * @memberof ImageLayer
+   */
   getBounds(): Bounds
 
+
+  /**
+   * 设置ImageLayer显示的范围
+   *
+   * @param {Bounds} bounds
+   * @memberof ImageLayer
+   */
   setBounds(bounds: Bounds)
 
+
+  /**
+   * 显示
+   *
+   * @memberof ImageLayer
+   */
   show()
 
+
+  /**
+   * 隐藏
+   *
+   * @memberof ImageLayer
+   */
   hide()
 
+
+  /**
+   * 设置层级
+   *
+   * @param {Number} zindex
+   * @memberof ImageLayer
+   */
   setzIndex(zindex: Number)
 
+
+  /**
+   * 获取层级
+   *
+   * @returns {Number}
+   * @memberof ImageLayer
+   */
   getzIndex(): Number
 
+
+  /** 
+   * 返回Image对象
+   *
+   * @returns {HTMLCanvasElement}
+   * @memberof ImageLayer
+   */
   getElement(): HTMLCanvasElement
 
+
+  /**
+   * 修改Image的Url
+   *
+   * @param {String} str
+   * @memberof ImageLayer
+   */
   setImageUrl(str: String)
 
+
+  /**
+   * 返回Image的Url
+   *
+   * @returns {String}
+   * @memberof ImageLayer
+   */
   getImageUrl(): String
 
 }
--- ImageLayer/ImageLayerOptions.d.ts
+++ ImageLayer/ImageLayerOptions.d.ts
@@ -2,6 +2,18 @@ import { Bounds } from "../Bounds";
 import { Map } from "../Map/Map";
 
 export class ImageLayerOptions{
+
+  /**
+   *Creates an instance of ImageLayerOptions.
+   * @param {Bounds} bounds ImageLayer显示的范围
+   * @param {String} url 需要显示的Image的Url
+   * @param {Number} opacity 图层的透明度，[0,1]
+   * @param {Boolean} visible 是否显示
+   * @param {Map} map 图层所属的地图对象
+   * @param {Number} zIndex 层级，缺省为12
+   * @param {[Number,Number]} zooms 设置可见级别，[最小级别，最大级别]
+   * @memberof ImageLayerOptions
+   */
   constructor(
     bounds:Bounds,
     url:String,

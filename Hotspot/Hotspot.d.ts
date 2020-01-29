d42d281066fba08fd2f7cfca8e66124e45ebdd94
Aomd <g00665@163.com>
2020-01-29 21:28:49 +0800

    完善 Hotspot

--- Hotspot/Hotspot.d.ts
+++ Hotspot/Hotspot.d.ts
@@ -1,3 +1,16 @@
+import { Map } from "../Map/Map";
+type HotspotEventNames = 
+'click'|
+'mouseover'|
+'mouseout';
+
+
 export class Hotspot{
-  
+  constructor(
+      map:Map
+  )
+
+  setMap(map:Map)
+
+  on(event: HotspotEventNames, cb: Function)
 }

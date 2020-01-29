4bbb58c62ffa9dfdf72a6203515676a12ed87940
Aomd <g00665@163.com>
2020-01-29 21:27:44 +0800

    完善 Geocoder

--- Geocoder/Geocoder.d.ts
+++ Geocoder/Geocoder.d.ts
@@ -1,3 +1,47 @@
-export class Geocoder{
-  
+
+/**
+ * 地理编码与逆地理编码服务，用于地址描述与坐标间的相互转换
+ *
+ * @export
+ * @class Geocoder
+ */
+
+import { GeocoderOptions } from './GeocoderOptions'
+import { LngLat } from '../LngLat';
+
+type GeocoderEventNames = 'complete'|'error';
+
+export class Geocoder {
+    constructor(opts: GeocoderOptions|Object)
+
+
+    /** 
+     * 根据给定的地址描述进行解析，支持中文、拼音
+     *
+     * @param {String} address
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof Geocoder
+     */
+    getLocation(address: String, callback: (status: String, result: Object) => void)
+
+
+    /**
+     * 地理编码时，设置地址描述所在城市
+     *
+     * @param {String} city
+     * @memberof Geocoder
+     */
+    setCity(city: String)
+
+
+    /**
+     * 根据给定坐标进行解析
+     *
+     * @param {(LngLat | Array<LngLat>)} location
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof Geocoder
+     */
+    getAddress(location: LngLat | Array<LngLat>, callback: (status: String, result: Object) => void)
+
+    on(event: GeocoderEventNames, cb: Function)
 }
--- Geocoder/GeocoderOptions.d.ts
+++ Geocoder/GeocoderOptions.d.ts
@@ -1,3 +1,19 @@
 export class GeocoderOptions{
-  
+
+  /**
+   *Creates an instance of GeocoderOptions.
+   * @param {String} city 城市，地理编码时，设置地址描述所在城市
+   * @param {Number} radius 逆地理编码时，以给定坐标为中心点，单位：米
+   * @param {String} lang 设置语言类型
+   * @param {Boolean} batch 是否批量查询batch=true为批量查询,batch=false为单点查询，
+   * @param {String} extensions 逆地理编码时，返回信息的详略
+   * @memberof GeocoderOptions
+   */
+  constructor(
+    city:String,
+    radius:    Number,
+    lang:String,
+    batch:Boolean,
+    extensions:String
+  )
 }

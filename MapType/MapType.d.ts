6e37caf8802f1bb7654397a71f0308da7e117b2f
Aomd <g00665@163.com>
2020-01-30 00:43:31 +0800

    完善 MapType

--- MapType/MapType.d.ts
+++ MapType/MapType.d.ts
@@ -1,3 +1,23 @@
-export class MapType{
-  
+import { MaptypeOptions } from './MaptypeOptions'
+
+export class MapType {
+    constructor(
+        opt: MaptypeOptions | Object
+    )
+
+
+    /**
+     * 显示地图类型切换控件
+     *
+     * @memberof MapType
+     */
+    show( )
+
+
+    /**
+     * 隐藏地图类型切换控件
+     *
+     * @memberof MapType
+     */
+    hide()
 }
--- MapType/MaptypeOptions.d.ts
+++ MapType/MaptypeOptions.d.ts
@@ -1,3 +1,21 @@
 export class MaptypeOptions{
-  
+
+
+  /**
+   *Creates an instance of MaptypeOptions.
+   * @param {Number} defaultType 初始化默认图层类型。 取值为0：默认底图 取值为1：卫星图 默认值：0
+   * @param {Boolean} showTraffic 叠加实时交通图层 默认值：false
+   * @param {Boolean} showRoad 叠加路网图层 默认值：false
+   * @memberof MaptypeOptions
+   */
+  constructor(
+    defaultType:Number,
+    
+    
+    showTraffic:Boolean,
+    
+    
+    showRoad:Boolean,
+    
+  )
 }

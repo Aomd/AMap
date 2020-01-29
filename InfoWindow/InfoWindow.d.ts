c88bfb22b6f4513ac7c339a2dad0da58b6948456
Aomd <g00665@163.com>
2020-01-30 00:41:51 +0800

    完善 InfoWindow

--- InfoWindow/InfoWindow.d.ts
+++ InfoWindow/InfoWindow.d.ts
@@ -1,3 +1,116 @@
-export class InfoWindow{
-  
+import { InfoWindowOptions } from './InfoWindowOptions'
+import { Map } from '../Map/Map';
+import { LngLat } from '../LngLat';
+import { Size } from '../Size';
+
+
+type InfoWindowEventNames =
+    'change' |
+    'open' |
+    'close';
+
+export class InfoWindow {
+    constructor(opt: InfoWindowOptions | Object)
+
+
+    /**
+     * 在地图的指定位置打开信息窗体
+     *
+     * @param {Map} map
+     * @param {LngLat} pos
+     * @memberof InfoWindow
+     */
+    open(map: Map, pos: LngLat)
+
+
+    /**
+     * 关闭信息窗体
+     *
+     * @memberof InfoWindow
+     */
+    close()
+
+
+    /**
+     * 获取信息窗体是否打开
+     *
+     * @returns {Boolean}
+     * @memberof InfoWindow
+     */
+    getIsOpen(): Boolean
+
+
+    /**
+     * 设置信息窗体内容，可通过该函数动态更新信息窗体中的信息
+     *
+     * @param {(String|HTMLElement)} content
+     * @memberof InfoWindow
+     */
+    setContent(content: String | HTMLElement)
+
+
+    /**
+     * 获取信息窗体内容 ，结果以字符串方式返回
+     *
+     * @returns {String}
+     * @memberof InfoWindow
+     */
+    getContent(): String
+
+
+    /**
+     * 设置信息窗体显示基点位置
+     *
+     * @param {LngLat} lnglat
+     * @memberof InfoWindow
+     */
+    setPosition(lnglat: LngLat)
+
+
+    /**
+     * 获取信息窗体显示基点位置
+     *
+     * @returns {LngLat}
+     * @memberof InfoWindow
+     */
+    getPosition(): LngLat
+
+
+    /**
+     * 获取信息窗体锚点          
+     *  
+     * @returns {String}
+     * @memberof InfoWindow
+     */
+    getAnchor(): String
+
+
+    /**
+     * 设置信息窗体锚点
+     *
+     * @memberof InfoWindow
+     */
+    setAnchor()
+
+
+    /**
+     * 设置信息窗体大小
+     *
+     * @param {Size} size
+     * @memberof InfoWindow
+     */
+    setSize(size: Size)
+
+
+    /**
+     * 获取信息窗体大小
+     *
+     * @returns {Size}
+     * @memberof InfoWindow
+     */
+    getSize(): Size
+
+
+    on(event: InfoWindowEventNames, cb: Function)
+
 }
--- InfoWindow/InfoWindowOptions.d.ts
+++ InfoWindow/InfoWindowOptions.d.ts
@@ -1,3 +1,34 @@
+import { Size } from "../Size";
+import { Pixel } from "../Pixel";
+import { LngLat } from "../LngLat";
+
 export class InfoWindowOptions{
-  
+
+
+  /**
+   *Creates an instance of InfoWindowOptions.
+   * @param {Boolean} isCustom 是否自定义窗体。设为true时
+   * @param {Boolean} autoMove 是否自动调整窗体到视野内
+   * @param {Boolean} closeWhenClickMap 控制是否在鼠标点击地图后关闭信息窗体，默认false，鼠标点击地图后不关闭信息窗体
+   * @param {(String|HTMLElement)} content 显示内容，可以是HTML要素字符串或者HTMLElement对象
+   * @param {Size} size 信息窗体尺寸（isCustom为true时，该属性无效）
+   * @param {String} anchor 信息窗体锚点。
+   * @param {Pixel} offset 信息窗体显示位置偏移量。默认基准点为信息窗体的底部中心
+   * @param {LngLat} position 信息窗体显示基点位置
+   * @param {Boolean} showShadow Boolean 控制是否显示信息窗体阴影，取值false时不显示窗体阴影，取值true时显示窗体阴影默认值：false
+   * @param {Boolean} retainWhenClose 信息窗体关闭时，是否将其Dom元素从页面中移除，默认为false
+   * @memberof InfoWindowOptions
+   */
+  constructor(
+    isCustom:Boolean,
+    autoMove:Boolean,
+    closeWhenClickMap:Boolean,
+    content:String|HTMLElement,
+    size:Size,
+    anchor:String,
+    offset:Pixel,
+    position:LngLat,
+    showShadow:Boolean,
+    retainWhenClose:Boolean
+  )
 }

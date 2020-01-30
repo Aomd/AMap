78368de34d6f7264092e300a3637902442eb646f
Aomd <g00665@163.com>
2020-01-30 15:51:30 +0800

    完善 Walking

--- Walking/Walking.d.ts
+++ Walking/Walking.d.ts
@@ -1,3 +1,40 @@
-export class Walking{
-  
+import { WalkingOptions } from './WalkingOptions'
+import { LngLat } from '../LngLat'
+
+
+type WalkingEventNames = 
+'complete' |
+'error' |
+'searchOnAMAP';
+/**
+ *
+ *
+ * @export
+ * @class Walking
+ */
+export class Walking {
+    constructor(
+        opts: WalkingOptions | Object
+    )
+
+
+    /**
+     * 根据起点和终点坐标
+     *
+     * @param {LngLat} origin
+     * @param {LngLat} destination
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof Walking
+     */
+    search(origin: LngLat, destination: LngLat, callback: (status: String, result: Object) => void)
+
+
+    /**
+     * 清除搜索的结果
+     *
+     * @memberof Walking
+     */
+    clear()
+
+    on(event: WalkingEventNames, cb: Function)
 }
--- Walking/WalkingOptions.d.ts
+++ Walking/WalkingOptions.d.ts
@@ -1,3 +1,23 @@
+import { Map } from "../Map/Map";
+
 export class WalkingOptions{
-  
+
+  /**
+   *Creates an instance of WalkingOptions.
+   * @param {Map} map 展现结果的地图实例
+   * @param {(String|HTMLElement)} panel 结果列表的HTML容器id或容器元素
+   * @param {Boolean} hideMarkers 设置隐藏路径规划的起始点图标
+   * @param {Boolean} isOutline 使用map属性时，绘制的规划线路是否显示描边
+   * @param {String} outlineColor 使用map属性时，绘制的规划线路的描边颜色
+   * @param {Boolean} autoFitView 用于控制在路径规划结束后，是否自动调整地图视野使绘制的路线处于视口的可见范围
+   * @memberof WalkingOptions
+   */
+  constructor(
+    map:Map,
+    panel:String|HTMLElement,
+    hideMarkers:Boolean,
+    isOutline:Boolean,
+    outlineColor:String,
+    autoFitView:Boolean
+  )
 }

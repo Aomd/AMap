459829eb028e7800339804ca2415f10cfd42ed92
Aomd <g00665@163.com>
2020-01-30 15:49:41 +0800

    完善 Rectangle

--- Rectangle/Rectangle.d.ts
+++ Rectangle/Rectangle.d.ts
@@ -1,3 +1,122 @@
-export class Rectangle{
-  
+import { RectangleOptions } from './RectangleOptions'
+import { Bounds } from '../Bounds';
+import { Map } from '../Map/Map';
+import { LngLat } from '../LngLat';
+
+
+type RectangleEventNames =
+
+  'click' |
+  'dblclick' |
+  'rightclick' |
+  'hide' |
+  'show' |
+  'mousedown' |
+  'mouseup' |
+  'mouseover' |
+  'mouseout' |
+  'change' |
+  'touchstart' |
+  'touchmove' |
+  'touchend';
+
+/**
+ *
+ *
+ * @export
+ * @class Rectangle
+ */
+export class Rectangle {
+    constructor(
+        opt: RectangleOptions | Object
+    )
+
+    /**
+     * 获取矩形范围
+     *
+     * @returns {Bounds}
+     * @memberof Rectangle
+     */
+    getBounds(): Bounds
+    
+    
+    /**
+     * 设置矩形的范围
+     *
+     * @param {Bounds} bounds
+     * @memberof Rectangle
+     */
+    setBounds(bounds: Bounds)
+    
+    
+    /**
+     * 修改矩形属性
+     *
+     * @param {RectangleOptions} rectangleopt
+     * @memberof Rectangle
+     */
+    setOptions(rectangleopt: RectangleOptions)
+    
+    
+    /**
+     * 获取矩形的属性
+     *
+     * @returns {Object}
+     * @memberof Rectangle
+     */
+    getOptions(): Object
+    
+    
+    /**
+     * 地图上隐藏矩形
+     *
+     * @memberof Rectangle
+     */
+    hide()
+    
+    
+    /**
+     * 地图上显示矩形
+     *
+     * @memberof Rectangle
+     */
+    show()
+    
+    
+    /**
+     * 在指定地图上添加该矩形覆盖物
+     *
+     * @param {Map} map
+     * @memberof Rectangle
+     */
+    setMap(map: Map)
+    
+    
+    /**
+     * 设置用户自定义属性
+     *
+     * @param {*} ext
+     * @memberof Rectangle
+     */
+    setExtData(ext: any)
+    
+    /**
+     * 获取用户自定义属性
+     *
+     * @returns {*}
+     * @memberof Rectangle
+     */
+    getExtData(): any
+
+
+    /**
+     * 判断指定点坐标是否在矩形内
+     *
+     * @param {LngLat} point
+     * @returns {Boolean}
+     * @memberof Rectangle
+     */
+    contains(point: LngLat): Boolean
+
+    on(event: RectangleEventNames, cb: Function)
 }
--- Rectangle/RectangleOptions.d.ts
+++ Rectangle/RectangleOptions.d.ts
@@ -1,3 +1,39 @@
-export class RectangleOptions{
-  
+import { Map } from "../Map/Map";
+import { Bounds } from "../Bounds";
+
+export class RectangleOptions {
+
+
+    /**
+     *Creates an instance of RectangleOptions.
+     * @param {Map} map 要显示该rectangle的地图对象
+     * @param {Number} zIndex 层叠顺序默认zIndex:10
+     * @param {Bounds} bounds 矩形的范围
+     * @param {Boolean} bubble 是否将覆盖物的鼠标或touch等事件冒泡到地图上
+     * @param {String} cursor 指定鼠标悬停时的鼠标样式
+     * @param {String} strokeColor 线条颜色
+     * @param {Number} strokeOpacity 轮廓线透明度
+     * @param {Number} strokeWeight 轮廓线宽度
+     * @param {String} fillColor 矩形填充颜色,使用16进制颜色代码赋值
+     * @param {Number} fillOpacity 矩形填充透明度
+     * @param {String} strokeStyle 轮廓线样式
+     * @param {*} extData 用户自定义属性
+     * @param {Array<Number>} strokeDasharray 勾勒形状轮廓的虚线和间隙的样式
+     * @memberof RectangleOptions
+     */
+    constructor(
+        map: Map,
+        zIndex: Number,
+        bounds: Bounds,
+        bubble: Boolean,
+        cursor: String,
+        strokeColor: String,
+        strokeOpacity: Number,
+        strokeWeight: Number,
+        fillColor: String,
+        fillOpacity: Number,
+        strokeStyle: String,
+        extData: any,
+        strokeDasharray: Array<Number>,
+    )
 }

70bab1216aa1149f5389828622563b8572e9839a
Aomd <g00665@163.com>
2020-01-30 15:51:21 +0800

    完善 VideoLayer

--- VideoLayer/VideoLayer.d.ts
+++ VideoLayer/VideoLayer.d.ts
@@ -6,21 +6,82 @@ export class VideoLayer{
     opts:VideoLayerOptions|Object
   )
 
+
+  /**
+   * 设置图层所属的地图对象
+   *
+   * @param {(Map|null)} map
+   * @memberof VideoLayer
+   */
   setMap(map:Map|null)
 
+
+  /**
+   * 返回图层所属的地图对象
+   *
+   * @returns {Map}
+   * @memberof VideoLayer
+   */
   getMap():Map
 
+
+  /**
+   * 显示
+   *
+   * @memberof VideoLayer
+   */
   show()
 
+
+  /**
+   * 隐藏
+   *
+   * @memberof VideoLayer
+   */
   hide()
 
+
+  /**
+   * 设置层级
+   *
+   * @param {Number} zindex
+   * @memberof VideoLayer
+   */
   setzIndex(zindex:Number)
 
+
+  /**
+   * 获取层级
+   *
+   * @returns {Number}
+   * @memberof VideoLayer
+   */
   getzIndex():Number
 
+
+  /**
+   * 返回Video对象
+   *
+   * @returns {HTMLVideoElement}
+   * @memberof VideoLayer
+   */
   getElement():HTMLVideoElement
 
+
+  /**
+   * 修改Video的Url
+   *
+   * @param {(String|[String])} str
+   * @memberof VideoLayer
+   */
   setVideoUrl(str:String|[String])
 
+
+  /**
+   * 返回Video的Url
+   *
+   * @returns {String}
+   * @memberof VideoLayer
+   */
   getVideoUrl():String
 }
--- VideoLayer/VideoLayerOptions.d.ts
+++ VideoLayer/VideoLayerOptions.d.ts
@@ -2,6 +2,20 @@ import { Bounds } from "../Bounds";
 import { Map } from "../Map/Map";
 
 export class VideoLayerOptions {
+
+  /**
+   *Creates an instance of VideoLayerOptions.
+   * @param {Boolean} autoplay 加载完成是否自动播放
+   * @param {Boolean} loop 是否循环播放
+   * @param {Bounds} bounds VideoLayer覆盖的范围
+   * @param {(String|Array<String>)} url 需要显示的Video的Url
+   * @param {Number} opacity 图层的透明度
+   * @param {Boolean} visible 是否显示
+   * @param {Map} map 图层所属的地图对象
+   * @param {Number} zIndex 层级，缺省为12
+   * @param {[Number,Number]} zooms 设置可见级别，[最小级别，最大级别]
+   * @memberof VideoLayerOptions
+   */
   constructor(
     autoplay:Boolean,
     loop:Boolean,

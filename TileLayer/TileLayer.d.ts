d81d3d0f206478231b7dc19af0ad6f29a1e82f7c
Aomd <g00665@163.com>
2020-01-30 15:50:44 +0800

    完善 TileLayer

--- TileLayer/TileLayer.d.ts
+++ TileLayer/TileLayer.d.ts
@@ -9,6 +9,9 @@ import { Traffic } from './Traffic/Traffic'
 
 import { Flexible } from './Flexible/Flexible'
 
+type TileLayerEventNames = 
+'complete'
+
 
 export class TileLayer {
 
@@ -25,14 +28,82 @@ export class TileLayer {
   constructor(
     tileOpt: Object | TileLayerOptions
   )
+
+  /**
+   * 设置图层透明度
+   *
+   * @param {Number} alpha
+   * @memberof TileLayer
+   */
   setOpacity(alpha: Number)
+  
+  
+  /**
+   * 在地图上显示该图层
+   *
+   * @memberof TileLayer
+   */
   show()
+  
+  
+  /**
+   * 隐藏图层
+   *
+   * @memberof TileLayer
+   */
   hide()
+  
+  
+  /**
+   *  获取当前图层所有切片号。
+   *
+   * @returns {Array<Number>}
+   * @memberof TileLayer
+   */
   getTiles(): Array<Number>
+  
+  
+  /**
+   * 重新加载此图层
+   *
+   * @memberof TileLayer
+   */
   reload()
+
+
+  /**
+   * 设置图层的取图地址
+   *
+   * @memberof TileLayer
+   */
   setTileUrl()
+  
+  
+  /**
+   * 获取该图层可显示的级别范围
+   *
+   * @returns {Array<Number>}
+   * @memberof TileLayer
+   */
   getZooms(): Array<Number>
+  
+  
+  /**
+   * 设置图层叠加的顺序值
+   *
+   * @param {Number} index
+   * @memberof TileLayer
+   */
   setzIndex(index: Number)
+  
+  /**
+   * 设置要显示图层的地图对象
+   *
+   * @param {Map} map
+   * @memberof TileLayer
+   */
   setMap(map: Map)
+
+  on(event: TileLayerEventNames, cb: Function)
 }
 

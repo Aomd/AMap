1dc89ac94bdbb3720f7cd90a1b05ca6f6b2d648d
Aomd <g00665@163.com>
2020-01-30 15:48:28 +0800

    完善 OverView

--- OverView/OverView.d.ts
+++ OverView/OverView.d.ts
@@ -1,3 +1,86 @@
-export class OverView{
-  
+import { OverViewOptions } from './OverViewOptions'
+import { TileLayer } from '../TileLayer/TileLayer';
+
+type OverViewEventNames =
+    'show' |
+    'hide' |
+    'open' |
+    'close';
+
+/**
+ * 地图鹰眼插件
+ *
+ * @export
+ * @class OverView
+ */
+export class OverView {
+    constructor(
+        opt: OverViewOptions | Object
+    )
+
+
+    /**
+     * 显示鹰眼窗体
+     *
+     * @memberof OverView
+     */
+    show()
+
+
+
+
+
+    /**
+     * 隐藏鹰眼窗体
+     *
+     * @memberof OverView
+     */
+    hide()
+
+
+
+
+
+    /**
+     * 展开鹰眼窗口
+     *
+     * @memberof OverView
+     */
+    open()
+
+
+
+
+
+    /**
+     *
+     *
+     * @memberof OverView
+     */
+    close()
+
+
+
+
+
+    /**
+     * 设置鹰眼中需显示的切片图层
+     *
+     * @param {*} TileLayer
+     * @memberof OverView
+     */
+    setTileLayer(TileLayer)
+
+
+
+
+
+    /**
+     * 获取窗体中显示的切片图层
+     *
+     * @memberof OverView
+     */
+    getTileLayer(): TileLayer | Object
+
+    on(event: OverViewEventNames, cb: Function)
 }
--- OverView/OverViewOptions.d.ts
+++ OverView/OverViewOptions.d.ts
@@ -1,3 +1,27 @@
-export class OverViewOptions{
-  
+import { TileLayer } from "../TileLayer/TileLayer";
+
+export class OverViewOptions {
+
+
+
+    /**
+     *Creates an instance of OverViewOptions.
+     * @param {TileLayer} tileLayer 鹰眼窗体中需显示的切片图层
+     * @param {Boolean} isOpen  鹰眼是否展开，默认为false
+     * @param {Boolean} visible 鹰眼是否显示，默认为true
+     * @memberof OverViewOptions
+     */
+    constructor(
+        tileLayer: TileLayer,
+
+        // 鹰眼窗体中需显示的切片图层
+
+        isOpen: Boolean,
+
+        // 鹰眼是否展开，默认为false
+
+        visible: Boolean,
+
+        // 鹰眼是否显示，默认为true
+    )
 }

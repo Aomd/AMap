68103189b4a785daff5350b42ebe948b6024d823
Aomd <g00665@163.com>
2020-01-30 15:49:00 +0800

    完善 PolyEditor

--- PolyEditor/PolyEditor.d.ts
+++ PolyEditor/PolyEditor.d.ts
@@ -1,3 +1,39 @@
+import { Map } from "../Map/Map";
+
+
+type PolyEditorEventNames =
+    'show' |
+    'hide' |
+    'open' |
+    'close';
+
+/**
+ * PolyEditor的构造函数，参数Map为指定的地图对象，Object为需要编辑的折线或多边形对象。目前仅支持桌面浏览器
+ *
+ * @export
+ * @class PolyEditor
+ */
 export class PolyEditor{
-  
+  constructor(
+    map:Map,obj:Object
+  )
+
+
+  /**
+   *
+   *
+   * @memberof PolyEditor
+   */
+  open( )
+
+
+  /**
+   *
+   *
+   * @memberof PolyEditor
+   */
+  close()
+
+
+  on(event: PolyEditorEventNames, cb: Function)
 }

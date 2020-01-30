b1bf7ab1f517f557bf4775b0260cf0f49341019f
Aomd <g00665@163.com>
2020-01-30 15:51:10 +0800

    完善 TruckDriving

--- TruckDriving/TruckDriving.d.ts
+++ TruckDriving/TruckDriving.d.ts
@@ -1,3 +1,40 @@
-export class TruckDriving{
-  
+import {TruckDrivingOptions} from './TruckDrivingOptions'
+
+/**
+ * 货车路线规划
+ *
+ * @export
+ * @class TruckDriving
+ */
+export class TruckDriving {
+    constructor(
+        opts:TruckDrivingOptions|Object
+    )
+
+    /**
+     * 根据路径，实现货车路线规划
+     *
+     * @param {Array<Number>} path
+     * @param {(status: String, result: Object) => void} cbk
+     * @memberof TruckDriving
+     */
+    search(path: Array<Number>, cbk: (status: String, result: Object) => void)
+
+
+    /**
+     * 清除搜索结果
+     *
+     * @memberof TruckDriving
+     */
+    clear()
+
+
+    /**
+     *  修改车牌号
+     *
+     * @param {String} province
+     * @param {Number} number
+     * @memberof TruckDriving
+     */
+    setProvinceAndNumber(province:String,number:Number)
 }
--- TruckDriving/TruckDrivingOptions.d.ts
+++ TruckDriving/TruckDrivingOptions.d.ts
@@ -1,3 +1,25 @@
+import { Map } from "../Map/Map";
+
 export class TruckDrivingOptions{
-  
+  constructor(
+    policy:Number,
+    size:Number,
+    width:Number,
+    height:Number,
+    load:Number,
+    weight:Number,
+    axlesNum:Number,
+
+    extensions:String,
+    map:Map,
+    panel:String|HTMLElement,
+    hideMarkers:Boolean,
+    showTraffic:Boolean,
+    isOutline:Boolean,
+    outlineColor:String,
+    autoFitView:Boolean,
+    province:String,
+    number:Number
+
+  )
 }

fccc74bc119cf0070372048e574e49ff7fc330a6
Aomd <g00665@163.com>
2020-01-30 15:49:23 +0800

    完善 Polyline

--- Polyline/Polyline.d.ts
+++ Polyline/Polyline.d.ts
@@ -1,3 +1,122 @@
-export class Polyline{
-  
+import { PolylineOptions } from "./PolylineOptions";
+import { Bounds } from "../Bounds";
+import { Map } from "../Map/Map";
+
+
+type PolylineEventNames = 
+
+'click'|
+'dblclick'|
+'rightclick'|
+'hide'|
+'show'|
+'mousedown'|
+'mouseup'|
+'mouseover'|
+'mouseout'|
+'change'|
+'touchstart'|
+'touchmove'|
+'touchend';
+
+/**
+ * 折线
+ *
+ * @export
+ * @class Polyline
+ */
+export class Polyline {
+    constructor(
+        opt: PolylineOptions | Object
+    )
+
+
+    /**
+     * 设置组成该折线的节点数组
+     *
+     * @param {Array<Number>} path
+     * @memberof Polyline
+     */
+    setPath(path: Array<Number>)
+
+
+    /**
+     * 获取折线路径的节点数组
+     *
+     * @returns {Array<Number>}
+     * @memberof Polyline
+     */
+    getPath(): Array<Number>
+
+    /**
+     * 修改折线属性
+     *
+     * @param {(PolylineOptions | Object)} opt
+     * @memberof Polyline
+     */
+    setOptions(opt: PolylineOptions | Object)
+
+    /**
+     * 获取线的属性
+     *
+     * @returns {Object}
+     * @memberof Polyline
+     */
+    getOptions(): Object
+
+    /**
+     * 获取折线的总长度
+     *
+     * @returns {Number}
+     * @memberof Polyline
+     */
+    getLength(): Number
+
+    /**
+     * 获取当前折线的矩形范围对象
+     *
+     * @returns {Bounds}
+     * @memberof Polyline
+     */
+    getBounds(): Bounds
+
+    /**
+     * 地图上隐藏指定折线
+     *
+     * @memberof Polyline
+     */
+    hide()
+
+    /**
+     * 地图上显示指定折线
+     *
+     * @memberof Polyline
+     */
+    show()
+
+    /**
+     * 设置折线所在的地图
+     *
+     * @param {Map} map
+     * @memberof Polyline
+     */
+    setMap(map: Map)
+
+    /**
+     * 设置用户自定义属性
+     *
+     * @param {*} ext
+     * @memberof Polyline
+     */
+    setExtData(ext: any)
+
+    /**
+     * 获取用户自定义属性
+     *
+     * @returns {*}
+     * @memberof Polyline
+     */
+    getExtData(): any
+
+    on(event: PolylineEventNames, cb: Function)
 }
--- Polyline/PolylineOptions.d.ts
+++ Polyline/PolylineOptions.d.ts
@@ -1,3 +1,55 @@
-export class PolylineOptions{
-  
+import { Map } from "../Map/Map"
+
+/**
+ *
+ *
+ * @export
+ * @class PolylineOptions
+ */
+export class PolylineOptions {
+
+    /**
+     *Creates an instance of PolylineOptions.
+     * @param {Map} map 要显示该polyline的地图对象
+     * @param {Number} zIndex 折线覆盖物的叠加顺序
+     * @param {Boolean} bubble 是否将覆盖物的鼠标或touch等事件冒泡到地图上
+     * @param {String} cursor 指定鼠标悬停时的鼠标样式
+     * @param {Boolean} geodesic 是否绘制成大地线
+     * @param {Boolean} isOutline 线条是否带描边
+     * @param {Number} borderWeight 描边的宽度
+     * @param {String} outlineColor 线条描边颜色
+     * @param {Array<Number>} path 折线的节点坐标数组
+     * @param {String} strokeColor 线条颜色
+     * @param {Number} strokeOpacity 线条透明度
+     * @param {Number} strokeWeight 线条宽度
+     * @param {String} strokeStyle 线样式
+     * @param {Array<Number>} strokeDasharray 勾勒形状轮廓的虚线和间隙的样式
+     * @param {String} lineJoin 折线拐点的绘制样式
+     * @param {String} lineCap 折线两端线帽的绘制样式
+     * @param {Boolean} draggable 设置折线是否可拖拽移动
+     * @param {*} extData 用户自定义属性
+     * @param {Boolean} showDir 是否延路径显示白色方向箭头
+     * @memberof PolylineOptions
+     */
+    constructor(
+        map: Map,
+        zIndex: Number,
+        bubble: Boolean,
+        cursor: String,
+        geodesic: Boolean,
+        isOutline: Boolean,
+        borderWeight: Number,
+        outlineColor: String,
+        path: Array<Number>,
+        strokeColor: String,
+        strokeOpacity: Number,
+        strokeWeight: Number,
+        strokeStyle: String,
+        strokeDasharray: Array<Number>,
+        lineJoin: String,
+        lineCap: String,
+        draggable: Boolean,
+        extData: any,
+        showDir: Boolean,
+    )
 }

ef1c557e35f63fc20ff3ad1d848d40b3be8a7485
Aomd <g00665@163.com>
2020-01-30 15:49:12 +0800

    完善 Polygon

--- Polygon/Polygon.d.ts
+++ Polygon/Polygon.d.ts
@@ -1,3 +1,144 @@
-export class Polygon{
-  
+
+import { PolygonOptions } from './PolygonOptions'
+import { LngLat } from '../LngLat';
+import { Bounds } from '../Bounds';
+import { Map } from '../Map/Map';
+
+
+
+type PolygonEventNames =
+
+  'click' |
+  'dblclick' |
+  'rightclick' |
+  'hide' |
+  'show' |
+  'mousedown' |
+  'mouseup' |
+  'mouseover' |
+  'mouseout' |
+  'change' |
+  'touchstart' |
+  'touchmove' |
+  'touchend';
+
+
+/**
+ * 多边形
+ *
+ * @export
+ * @class Polygon
+ */
+export class Polygon {
+  constructor(
+    opt: PolygonOptions | Object
+  )
+
+
+  /**
+   * 设置多边形轮廓线节点数组
+   *
+   * @param {(Array<LngLat>|Array<Array<LngLat>>)} path
+   * @memberof Polygon
+   */
+  setPath(path: Array<LngLat> | Array<Array<LngLat>>)
+
+
+  /**
+   * 获取多边形轮廓线节点数组
+   *
+   * @returns {(Array<LngLat>|Array<Array<LngLat>>)}
+   * @memberof Polygon
+   */
+  getPath(): Array<LngLat> | Array<Array<LngLat>>
+
+
+  /**
+   * 修改多边形属性
+   *
+   * @param {(PolygonOptions|Object)} opt
+   * @memberof Polygon
+   */
+  setOptions(opt: PolygonOptions | Object)
+
+  /**
+   * 获取多边形的属性
+   *
+   * @returns {Object}
+   * @memberof Polygon
+   */
+  getOptions(): Object
+
+  /**
+   * 获取当前多边形的矩形范围对象。
+   *
+   * @returns {Bounds}
+   * @memberof Polygon
+   */
+  getBounds(): Bounds
+
+
+  /**
+   * 获取多边形的面积
+   *
+   * @returns {Number}
+   * @memberof Polygon
+   */
+  getArea(): Number
+
+
+  /**
+   * 隐藏多边形
+   *
+   * @memberof Polygon
+   */
+  hide()
+
+
+  /**
+   * 显示多边形
+   *
+   * @memberof Polygon
+   */
+  show()
+
+
+  /**
+   * 在指定地图上显示当前的多边形
+   *
+   * @param {Map} map
+   * @memberof Polygon
+   */
+  setMap(map: Map)
+
+
+  /**
+   * 设置用户自定义属性
+   *
+   * @param {*} ext
+   * @memberof Polygon
+   */
+  setExtData(ext: any)
+
+
+  /**
+   * 获取用户自定义属性
+   *
+   * @returns {*}
+   * @memberof Polygon
+   */
+  getExtData(): any
+
+
+  /**
+   * 判断指定点坐标是否在多边形范围内
+   *
+   * @param {LngLat} point
+   * @returns {Boolean}
+   * @memberof Polygon
+   */
+  contains(point: LngLat): Boolean
+
+
+  on(event: PolygonEventNames, cb: Function)
 }
--- Polygon/PolygonOptions.d.ts
+++ Polygon/PolygonOptions.d.ts
@@ -1,3 +1,48 @@
-export class PolygonOptions{
-  
+import { Map } from "../Map/Map"
+import { LngLat } from "../LngLat";
+
+/**
+ *
+ *
+ * @export
+ * @class PolygonOptions
+ */
+export class PolygonOptions {
+
+    /**
+     *Creates an instance of PolygonOptions.
+     * @param {Map} map 要显示该polygon的地图对象
+     * @param {Number} zIndex 多边形覆盖物的叠加顺序
+     * @param {(Array<LngLat> | Array<Array<LngLat>>)} path 多边形轮廓线的节点坐标数组
+     * @param {Boolean} bubble 是否将覆盖物的鼠标或touch等事件冒泡到地图上
+     * @param {String} cursor 指定鼠标悬停时的鼠标样式
+     * @param {String} strokeColor 线条颜色
+     * @param {Number} strokeOpacity 轮廓线透明度，取值范围[0,1]
+     * @param {Number} strokeWeight 轮廓线宽度
+     * @param {String} fillColor 多边形填充颜色
+     * @param {Number} fillOpacity 多边形填充透明度
+     * @param {Boolean} draggable 设置多边形是否可拖拽移动
+     * @param {*} extData 用户自定义属性
+     * @param {String} strokeStyle 轮廓线样式
+     * @param {Array<Number>} strokeDasharray 勾勒形状轮廓的虚线和间隙的样式
+     * @memberof PolygonOptions
+     */
+    constructor(
+        map: Map,
+        zIndex: Number,
+        path: Array<LngLat> | Array<Array<LngLat>>,
+        bubble: Boolean,
+        cursor: String,
+        strokeColor: String,
+        strokeOpacity: Number,
+        strokeWeight: Number,
+        fillColor: String,
+        fillOpacity: Number,
+        draggable: Boolean,
+        extData: any,
+        strokeStyle: String,
+        strokeDasharray: Array<Number>,
+
+
+    )
 }

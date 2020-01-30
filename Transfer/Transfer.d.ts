1d123af7a01d0126d115ec95011f2d149c8f9002
Aomd <g00665@163.com>
2020-01-30 15:51:01 +0800

    完善 Transfer

--- Transfer/Transfer.d.ts
+++ Transfer/Transfer.d.ts
@@ -1,3 +1,114 @@
-export class Transfer{
-  
+import { TransferOptions } from './TransferOptions';
+import { LngLat } from '../LngLat';
+
+type TransferPolicy =
+
+    'LEAST_TIME' |
+
+
+
+    'LEAST_FEE' |
+
+
+
+    'LEAST_TRANSFER' |
+
+
+
+    'LEAST_WALK' |
+
+
+
+    'MOST_COMFORT' |
+
+
+
+    'NO_SUBWAY';
+
+type TransferEventNames =
+    'complete' |
+    'error'
+
+/**
+ * 公交换乘服务
+ *
+ * @export
+ * @class Transfer
+ */
+export class Transfer {
+    constructor(
+        opts: TransferOptions | Object
+    )
+
+
+    /**
+     * 根据起点和终点坐标/名称
+     *
+     * @param {LngLat} origin
+     * @param {LngLat} destination
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof Transfer
+     */
+    search(origin: LngLat, destination: LngLat, callback: (status: String, result: Object) => void)
+
+
+    /**
+     * 设置公交换乘策略
+     *
+     * @param {TransferPolicy} policy
+     * @memberof Transfer
+     */
+    setPolicy(policy: TransferPolicy)
+
+
+    /**
+     * 设置公交换乘查询的城市
+     *
+     * @param {String} city
+     * @memberof Transfer
+     */
+    setCity(city: String)
+
+
+    /**
+     * 设置公交换乘查询的城市
+     *
+     * @param {String} city
+     * @memberof Transfer
+     */
+    setCityd(city: String)
+
+
+    /**
+     * 设置公交路径规划出发时间
+     *
+     * @param {String} time
+     * @param {String} data
+     * @memberof Transfer
+     */
+    leaveAt(time: String, data: String)
+
+
+
+    /**
+     * 清除结果显示
+     *
+     * @memberof Transfer
+     */
+    clear()
+
+
+
+    /**
+     * 唤起高德地图客户端公交路径规划
+     *
+     * @param {Object} obj
+     * @memberof Transfer
+     */
+    searchOnAMAP(obj: Object)
+
+
+    on(event: TransferEventNames, cb: Function)
 }
+
+
--- Transfer/TransferOptions.d.ts
+++ Transfer/TransferOptions.d.ts
@@ -1,3 +1,34 @@
+import { TransferPolicy } from "./Transfer";
+import { Map } from "../Map/Map";
+
 export class TransferOptions{
-  
+
+  /**
+   *Creates an instance of TransferOptions.
+   * @param {String} city 公交换乘的城市
+   * @param {TransferPolicy} policy 公交换乘策略
+   * @param {Boolean} nightflag 是否计算夜班车，默认为不计算
+   * @param {String} cityd 终点城市，跨城公交路径规划时为必填参数
+   * @param {String} extensions  返回结果控制
+   * @param {Map} map 展现结果的地图实例
+   * @param {(String|HTMLElement)} panel 结果列表的HTML容器id或容器元素
+   * @param {Boolean} hideMarkers 设置隐藏路径规划的起始点图标
+   * @param {Boolean} isOutline 使用map属性时，绘制的规划线路是否显示描边
+   * @param {String} outlineColor 使用map属性时，绘制的规划线路的描边颜色
+   * @param {Boolean} autoFitView 用于控制在路径规划结束后
+   * @memberof TransferOptions
+   */
+  constructor(
+    city:String,
+    policy:TransferPolicy,
+    nightflag:Boolean,
+    cityd:String,
+    extensions:String,
+    map:Map,
+    panel:String|HTMLElement,
+    hideMarkers:Boolean,
+    isOutline:Boolean,
+    outlineColor:String,
+    autoFitView:Boolean
+  )
 }

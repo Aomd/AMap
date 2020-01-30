37ff6e240298571e46cafdf889bdabbb59392441
Aomd <g00665@163.com>
2020-01-30 15:50:53 +0800

    完善 ToolBar

--- ToolBar/ToolBar.d.ts
+++ ToolBar/ToolBar.d.ts
@@ -1,3 +1,118 @@
-export class ToolBar{
-  
+import { ToolbarOptions } from './ToolbarOptions'
+import { Pixel } from '../Pixel';
+import { LngLat } from '../LngLat';
+
+
+type ToolBarEventNames = 
+'show'|
+'hide'|
+'location'|
+'zoomchanged'
+
+/**
+ * 地图操作工具条插件
+ *
+ * @export
+ * @class ToolBar
+ */
+export class ToolBar {
+  constructor(
+    opt: ToolbarOptions | Object
+  )
+
+
+  /**
+   * 获取工具条相对于地图容器左上角的偏移量
+   *
+   * @returns {Pixel}
+   * @memberof ToolBar
+   */
+  getOffset():Pixel 
+
+
+  /**
+   * 设置工具条相对于地图容器左上角的偏移量
+   *
+   * @param {Pixel} pixel
+   * @memberof ToolBar
+   */
+  setOffset(pixel: Pixel)
+
+
+  /**
+   * 隐藏缩放级别等级条
+   *
+   * @memberof ToolBar
+   */
+  hideRuler( )
+
+  /**
+   * 显示缩放级别等级条
+   *
+   * @memberof ToolBar
+   */
+  showRuler( )
+
+  /**
+   * 隐藏方向键盘
+   *
+   * @memberof ToolBar
+   */
+  hideDirection( )
+
+  /**
+   * 显示方向键盘
+   *
+   * @memberof ToolBar
+   */
+  showDirection( )
+
+
+  /**
+   * 隐藏定位小部件
+   *
+   * @memberof ToolBar
+   */
+  hideLocation( )
+
+
+  /** 
+   * 显示定位小部件
+   *
+   * @memberof ToolBar
+   */
+  showLocation( )
+
+
+  /**
+   * 进行位置定位
+   *
+   * @memberof ToolBar
+   */
+  doLocation( )
+
+  /**
+   *获取上次定位的结果
+   *
+   * @returns {LngLat}
+   * @memberof ToolBar
+   */
+  getLocation( ):LngLat
+
+  /**
+   * 隐藏工具条
+   *
+   * @memberof ToolBar
+   */
+  hide( )
+
+  /**
+   * 显示工具条
+   *
+   * @memberof ToolBar
+   */
+  show( )
+
+
+  on(event: ToolBarEventNames, cb: Function)
 }
--- ToolBar/ToolbarOptions.d.ts
+++ ToolBar/ToolbarOptions.d.ts
@@ -1,3 +1,33 @@
+import { Pixel } from "../Pixel";
+import { Marker } from "../Marker/Marker";
+
 export class ToolbarOptions{
-  
+
+  /**
+   *Creates an instance of ToolbarOptions.
+   * @param {Pixel} offset 相对于地图容器左上角的偏移量
+   * @param {String} position 控件停靠位置
+   * @param {Boolean} ruler 标尺键盘是否可见，默认为true
+   * @param {Boolean} noIpLocate 定位失败后，是否开启IP定位，默认为false
+   * @param {Boolean} locate 是否显示定位按钮，默认为false
+   * @param {Boolean} liteStyle 是否使用精简模式，默认为false
+   * @param {Boolean} direction 方向键盘是否可见，默认为true
+   * @param {Boolean} autoPosition 是否自动定位，即地图初始化加载完成后
+   * @param {Boolean} useNative 自定义定位图标，值为Marker对象
+   * @param {Marker} locationMarker 是否使用高德定位sdk用来辅助优化定位效果
+   * @memberof ToolbarOptions
+   */
+  constructor(
+    offset:Pixel,
+    position:String,
+    ruler:Boolean,
+    noIpLocate:Boolean,
+    locate:Boolean,
+    liteStyle:Boolean,
+    direction:Boolean,
+    autoPosition:Boolean,
+    useNative:Boolean,
+    locationMarker:Marker
+
+  )
 }

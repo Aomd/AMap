22c673200b748f43d93b25a49ed4d5d451451b6a
Aomd <g00665@163.com>
2020-01-30 15:49:57 +0800

    完善 Riding

--- Riding/Riding.d.ts
+++ Riding/Riding.d.ts
@@ -1,3 +1,42 @@
-export class Riding{
-  
+
+import { RidingOptions } from './RidingOptions'
+import { LngLat } from '../LngLat'
+
+
+type RidingEventNames = 
+
+'complete'|
+'error'
+
+/**
+ * 骑行路径规划服务
+ *
+ * @export
+ * @class Riding
+ */
+export class Riding {
+    constructor(
+        opts: RidingOptions | Object
+    )
+
+
+    /**
+     * 根据起点和终点坐标，实现骑行路径规划
+     *
+     * @param {LngLat} origin
+     * @param {LngLat} destination
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof Riding
+     */
+    search(origin: LngLat, destination: LngLat, callback: (status: String, result: Object) => void)
+
+
+    /**
+     * 清除搜索的结果
+     *
+     * @memberof Riding
+     */
+    clear()
+
+    on(event:RidingEventNames,cb:Function)
 }
--- Riding/RidingOptions.d.ts
+++ Riding/RidingOptions.d.ts
@@ -1,3 +1,25 @@
+import { Map } from "../Map/Map";
+
 export class RidingOptions{
-  
+
+  /**
+   *Creates an instance of RidingOptions.
+   * @param {Map} map 对象, 展现结果的地图实例
+   * @param {Number} policy 骑行路线规划策略
+   * @param {(String|HTMLElement)} panel 结果列表的HTML容器id或容器元素
+   * @param {Boolean} hideMarkers 设置隐藏路径规划的起始点图标
+   * @param {Boolean} isOutline 使用map属性时，绘制的规划线路是否显示描边
+   * @param {String} outlineColor 使用map属性时，绘制的规划线路的描边颜色
+   * @param {Boolean} autoFitView 用于控制在路径规划结束后
+   * @memberof RidingOptions
+   */
+  constructor(
+    map:Map,
+    policy:Number,
+    panel:String|HTMLElement,
+    hideMarkers:Boolean,
+    isOutline:Boolean,
+    outlineColor:String,
+    autoFitView:Boolean
+  )
 }

e0bb21e7ac5fc36c7d91e9aae1eed324ab3cd244
Aomd <g00665@163.com>
2020-01-30 15:49:48 +0800

    完善 RectangleEditor

--- RectangleEditor/RectangleEditor.d.ts
+++ RectangleEditor/RectangleEditor.d.ts
@@ -1,3 +1,25 @@
-export class RectangleEditor{
-  
+import { Map } from "../Map/Map";
+import { Rectangle } from "../Rectangle/Rectangle";
+
+
+type RectangleEditorEventNames =
+
+    'adjust' |
+    'end';
+/**
+ * 矩形的编辑插件
+ *
+ * @export
+ * @class RectangleEditor
+ */
+export class RectangleEditor {
+    constructor(
+        map: Map, Rectangle: Rectangle | Object
+    )
+
+    open()
+
+    close()
+
+    on(event: RectangleEditorEventNames, cb: Function)
 }

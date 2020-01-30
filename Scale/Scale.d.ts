b3ffecb0696a6762eaa63e804cfe09d15d80e8b1
Aomd <g00665@163.com>
2020-01-30 15:50:14 +0800

    完善 Scale

--- Scale/Scale.d.ts
+++ Scale/Scale.d.ts
@@ -1,3 +1,60 @@
+import { Pixel } from "../Pixel";
+
+
+
+
+
+type ScaleEventNames =
+    'complete' |
+    'error';
+
+
+
+/**
+ * 比例尺插件
+ *
+ * @export
+ * @class Scale
+ */
 export class Scale{
-  
+
+
+    /**
+     * 显示比例尺
+     *
+     * @memberof Scale
+     */
+    show( )
+    
+
+    /**
+     * 隐藏比例尺
+     *
+     * @memberof Scale
+     */
+    hide( )
+    
+    
+    
+    /**
+     * 相对于地图容器左上角的偏移量
+     *
+     * @returns {Pixel}
+     * @memberof Scale
+     */
+    offset():Pixel
+    
+    
+    
+    /**
+     * 控件停靠位置
+     *
+     * @returns {String}
+     * @memberof Scale
+     */
+    position():String      
+    
+    
+
+    on(event: ScaleEventNames, cb: Function)
 }

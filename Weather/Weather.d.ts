f85fb10a3ea9db511e001fb1cf1a74ef21a0909f
Aomd <g00665@163.com>
2020-01-30 15:51:36 +0800

    完善 Weather

--- Weather/Weather.d.ts
+++ Weather/Weather.d.ts
@@ -1,3 +1,34 @@
-export class Weather{
-  
+
+type WeatherEventNames =
+    'complete' |
+    'error'
+
+/**
+ *
+ *
+ * @export
+ * @class Weather
+ */
+export class Weather {
+
+    /**
+     * 查询实时天气信息
+     *
+     * @param {String} district
+     * @param {(obj: Object, objt: Object) => void} callback
+     * @memberof Weather
+     */
+    getLive(district: String, callback: (obj: Object, objt: Object) => void)
+
+
+    /**
+     * 查询四天预报天气
+     *
+     * @param {String} district
+     * @param {(obj: Object, objt: Object) => void} callback
+     * @memberof Weather
+     */
+    getForecast(district: String, callback: (obj: Object, objt: Object) => void)
+
+    on(event: WeatherEventNames, cb: Function)
 }

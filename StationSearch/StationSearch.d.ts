846c64f2fbbb4a8878128402a8308598139e8be1
Aomd <g00665@163.com>
2020-01-30 15:50:23 +0800

    完善 StationSearch

--- StationSearch/StationSearch.d.ts
+++ StationSearch/StationSearch.d.ts
@@ -1,3 +1,61 @@
-export class StationSearch{
-  
+
+import { StationSearchOptions } from './StationSearchOptions'
+
+/**
+ * 公交站点查询服务
+ *
+ * @export
+ * @class StationSearch
+ */
+export class StationSearch {
+    constructor(
+        opts: StationSearchOptions | Object
+    )
+
+
+    /**
+     * 根据给定的公交站点id进行公交站点详情检索
+     *
+     * @param {String} id
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof StationSearch
+     */
+    searchById(id: String, callback: (status: String, result: Object) => void)
+
+
+    /**
+     * 根据给定公交站点名称进行公交站点详情查询
+     *
+     * @param {String} keyword
+     * @param {(status: String, result: Object) => void} callback
+     * @memberof StationSearch
+     */
+    search(keyword: String, callback: (status: String, result: Object) => void)
+
+    /**
+     * 设置单页显示结果条数
+     *
+     * @param {Number} pageSize
+     * @memberof RoadInfoSearch
+     */
+    setPageSize(pageSize: Number)
+
+
+    /**
+     * 设置查询结果页码
+     *
+     * @param {Number} pageIndex
+     * @memberof RoadInfoSearch
+     */
+    setPageIndex(pageIndex: Number)
+
+
+    /**
+     * 设置查询城市
+     *
+     * @param {String} city
+     * @memberof RoadInfoSearch
+     */
+    setCity(city: String)
+
 }
--- StationSearch/StationSearchOptions.d.ts
+++ StationSearch/StationSearchOptions.d.ts
@@ -1,3 +1,14 @@
 export class StationSearchOptions{
-  
+    /**
+     *Creates an instance of StationSearchOptions.
+     * @param {Number} pageIndex 页码
+     * @param {Number} pageSize 单页显示结果条数
+     * @param {String} city公交站点所在城市
+     * @memberof StationSearchOptions
+     */
+    constructor(
+        pageIndex:Number,
+        pageSize:Number,
+        city:String
+    )
 }
